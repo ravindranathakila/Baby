6da0a917fb1e9486d1a821b6e6c4c7eff912fc8e
akila <akila>
2009-09-14 18:18:02 +0000

    20090914 Initial Commit

---
+++ src/main/java/ai/ilikeplaces/doc/InterfacePreamble.java
@@ -0,0 +1,85 @@
+package ai.ilikeplaces.doc;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * Make sure you annotate all classes with this annotation.<br>
+ * A Template is given below.<br>
+ * <blockquote><pre>
+\@ClassPreamble(
+    authors = {"Ravindranath Akila"},
+    version = 1,
+    description = {""},
+    conventions = {""},
+    implementations={Class.class},
+    notes = {"Initial Implementation"},
+    TODO =  {"Under Progress"})
+    </pre></blockquote>
+ *
+ * @author Ravindranath Akila
+ *
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface InterfacePreamble {
+
+    /**
+     *
+     * @return String array of authors
+     */
+    String[] authors();
+
+    /**
+     *
+     * @return Version number
+     */
+    int version();
+
+    /**
+     *
+     * @return String array of description
+     */
+    String[] description();
+
+    /**
+     *
+     * @return String array of conventions
+     */
+    String[] conventions() default {"Always specify some convention details"};
+
+    /**
+     *
+     * @return List of classes which implement this interface
+     */
+    Class[] implementations();
+
+    /**
+     *
+     * @return List of interfaces which extend this interface
+     */
+    Class[] extentions();
+
+    /**
+     *
+     * @return List of IMPORTANT classes which instanciate this class
+     */
+    Class[] instances() default {};
+
+    /**
+     *
+     * @return String array of notes
+     */
+    String[] notes() default {"No notes"};
+
+    /**
+     *
+     * @return String array of todo
+     */
+    String[] TODO() default {"Nothing left TODO"};
+}
---
+++ src/main/java/ai/ilikeplaces/doc/MethodPreamble.java
@@ -0,0 +1,79 @@
+package ai.ilikeplaces.doc;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * Make sure you annotate all methods with this annotation.<br>
+ * A Template is given below.<br>
+ * <blockquote><pre>
+\@MethodPreamble(
+    authors = {"Ravindranath Akila"},
+    version = 1,
+    description = {""},
+    conventions = {""},
+    callBackModules = {Class.class},
+    notes = {"Initial Implementation"},
+    TODO =  {"Under Implementation"})
+    </pre></blockquote>
+ * 
+ * @author Ravindranath Akila
+ * 
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface MethodPreamble {
+
+   /**
+     *
+     * @return String array of authors
+     */
+    String[] authors();
+
+    /**
+     *
+     * @return Version number
+     */
+    int version() default 0;
+
+    /**
+     *
+     * @return String array of description
+     */
+    String[] description();
+
+    /**
+     *
+     * @return Array of classes which use this method
+     */
+    Class[] callBackModules();
+
+    /**
+     *
+     * @return String array of conventions
+     */
+    String[] conventions() default {"Always specify some convention details"};
+
+    /**
+     *
+     * @return String array of reviewers
+     */
+    String[] reviewers() default {"No reviewers"};
+
+    /**
+     *
+     * @return String array of notes
+     */
+    String[] notes() default {"No notes"};
+
+    /**
+     *
+     * @return String array of todo
+     */
+    String[] TODO() default {"Nothing left TODO"};
+}
