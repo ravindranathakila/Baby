0b9059014544ba9df3a925e9bdb1c7633e76c529
akila <akila>
2009-09-14 18:24:55 +0000

    20090914 Many Changes. Controller was changed to hanlde photo description widget which was changed a lot including the abstract class of it

--- src/main/java/ai/ilikeplaces/ListenerMain.java
+++ src/main/java/ai/ilikeplaces/ListenerMain.java
@@ -5,12 +5,15 @@ import java.util.logging.Logger;
 import javax.naming.NamingException;
 import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.ItsNatServletResponse;
+import org.itsnat.core.http.ItsNatHttpSession;
 import org.itsnat.core.event.ItsNatServletRequestListener;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 import ai.ilikeplaces.widgets.*;
 import ai.ilikeplaces.entities.*;
+import ai.ilikeplaces.servlets.ServletLogin;
+import java.util.List;
 import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -22,8 +25,7 @@ import javax.naming.InitialContext;
 public class ListenerMain implements ItsNatServletRequestListener {
 
     private CrudServiceLocal<Location> crudServiceLocal;
-    final protected static String JsCodeToSend = "document.monitor = new EventMonitor(); \n"
-                                                +"document.getItsNatDoc().addEventMonitor(document.monitor); \n";
+    final protected static String JsCodeToSend = "document.monitor = new EventMonitor(); \n" + "document.getItsNatDoc().addEventMonitor(document.monitor); \n";
 
     @SuppressWarnings("unchecked")
     public ListenerMain() {
@@ -37,8 +39,10 @@ public class ListenerMain implements ItsNatServletRequestListener {
         }
     }
 
-    public void processRequest(final ItsNatServletRequest request_, final ItsNatServletResponse response_) {
-        new AbstractListener(request_) {
+    @Override
+    public void processRequest(final ItsNatServletRequest request__, final ItsNatServletResponse response__) {
+
+        new AbstractListener(request__) {
 
             /**
              * Intialize your document here by appending fragments
@@ -51,21 +55,27 @@ public class ListenerMain implements ItsNatServletRequestListener {
                     final String locationName_ = location_;
                     final String locationInfo_ = location_ + " is a now registered on www.ilikeplaces.com." + System.currentTimeMillis();
 
-                    final Location loc = new Location();
+                    final Location loc_ = new Location();
 
-                    loc.setLocationName(locationName_);
-                    loc.setLocationInfo(locationInfo_);
+                    loc_.setLocationName(locationName_);
+                    loc_.setLocationInfo(locationInfo_);
 
-                    final Location existingLocation = crudServiceLocal.find(Location.class, locationName_);
+                    final List<Location> existingLocations = crudServiceLocal.findWithNamedQuery(Location.FindAllLocationsByName,
+                            QueryParameter.with("locationName", loc_.getLocationName()).parameters());
+
+                    Location existingLocation = existingLocations.size() == 1 ? existingLocations.get(0) : null;
+
                     if (existingLocation == null) {
-                        crudServiceLocal.create(loc);
+                        crudServiceLocal.create(loc_);
                         getElementById("Main_temp1").appendChild(itsNatDocument_.getDocument().createTextNode("Create an entry for this location"));
 
                     } else {
                         //Kandy 1252410471093 Sigiriya 1252410457640
                         //Location result = (Location) manager.createQuery("SELECT location FROM Location location WHERE location.locationName = :locationName").setParameter("locationName", location_).getSingleResult();
-                        Location result = crudServiceLocal.find(Location.class, locationName_);
-                        getElementById("Main_temp1").appendChild(itsNatDocument_.getDocument().createTextNode(result.getLocationInfo()));
+
+                        existingLocation = existingLocations.size() == 1 ? existingLocations.get(0) : null;
+
+                        getElementById("Main_temp1").appendChild(itsNatDocument_.getDocument().createTextNode(existingLocation.getLocationInfo()+":"+existingLocation.getLocationSuperSet()+":"+existingLocation.toString(true)));
                     }
 
                 }
@@ -74,11 +84,15 @@ public class ListenerMain implements ItsNatServletRequestListener {
             /**
              * Use ItsNatHTMLDocument variable stored in the AbstractListener class
              */
+            @Override
             protected void registerEventListeners() {
                 /*Abstract implementation*/
                 itsNatHTMLDocument_.addEventListener((EventTarget) getElementById("Main_temp1"), Click, new EventListener() {
 
+                    @Override
                     public void handleEvent(final Event evt_) {
+                        final String userId = (String) itsNatHttpSession.getAttribute(ServletLogin.User);
+                        getElementById("Main_sidebar").appendChild(itsNatDocument_.getDocument().createTextNode(userId == null ? "NULL" : userId));
                         getElementById("Main_sidebar").appendChild(itsNatDocument_.getDocument().createTextNode("COOL! "));
                         new Photo$Description(itsNatDocument_, getElementById("Main_temp1")) {
                         };
@@ -88,6 +102,7 @@ public class ListenerMain implements ItsNatServletRequestListener {
                 /*Abstract implementation*/
                 itsNatHTMLDocument_.addEventListener((EventTarget) getElementById("Main_temp2"), Click, new EventListener() {
 
+                    @Override
                     public void handleEvent(final Event evt_) {
                         getElementById("Main_sidebar").appendChild(itsNatDocument_.getDocument().createTextNode("HOT! "));
                         new Photo$Description(itsNatDocument_, getElementById("Main_temp2")) {
