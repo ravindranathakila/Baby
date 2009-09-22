89536a384961461a498c1f8d73a3536c5d90fae1
akila <akila>
2009-09-22 16:35:21 +0000

    20090922 new

---
+++ src/main/java/ai/ilikeplaces/AbstractSBCallbacks.java
@@ -0,0 +1,27 @@
+package ai.ilikeplaces;
+
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ *
+ * @author Ravindranath Akila
+ */
+public class AbstractSBCallbacks {
+
+    final protected Logger logger = Logger.getLogger(this.getClass().getName());
+    final protected String className = this.getClass().getName();
+    final static private String MsgConstruct = "HELLO, I JUST CONSTRUCTED AN INSTANCE OF ";
+    final static private String MsgDestroy = "HELLO, I JUST DESTROYED AN INSTANCE OF ";
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info(MsgConstruct + className);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info(MsgDestroy + className);
+    }
+}
