59da57d1c986f1c88bf70d30500aadaaafdb99bf
akila <akila>
2009-09-24 14:44:21 +0000

    20090924 intialization methodology but seems unnecessary. Remove if performance degrades.

--- src/main/java/ai/ilikeplaces/AbstractListener.java
+++ src/main/java/ai/ilikeplaces/AbstractListener.java
@@ -1,6 +1,8 @@
 package ai.ilikeplaces;
 
+import ai.ilikeplaces.exception.ExceptionConstructorInvokation;
 import ai.ilikeplaces.servlets.Controller;
+import ai.ilikeplaces.servlets.ServletLogin;
 import java.util.Map;
 import org.itsnat.core.ItsNatDocument;
 import org.itsnat.core.ItsNatServlet;
@@ -21,23 +23,42 @@ public abstract class AbstractListener {
     /**
      * Use this ItsNatDocument in your code for handling the page
      */
-    protected final ItsNatDocument itsNatDocument_;
-    protected final ItsNatHTMLDocument itsNatHTMLDocument_;
-    protected final HTMLDocument hTMLDocument_;
-    protected final ItsNatServlet itsNatServlet_;
-    protected final ItsNatHttpSession itsNatHttpSession;
-    protected final String location_;
-    protected final static String Click = "click";
+    final protected ItsNatDocument itsNatDocument_;
+    final protected ItsNatHTMLDocument itsNatHTMLDocument_;
+    final protected HTMLDocument hTMLDocument_;
+    final protected ItsNatServlet itsNatServlet_;
+    final protected  ItsNatHttpSession itsNatHttpSession;
+    final protected String location_;
+    final static protected String Click = "click";
+    final protected SBLoggedOnUserFace sBLoggedOnUserFace;
 
     public AbstractListener(final ItsNatServletRequest request_) {
-        this.itsNatDocument_ = request_.getItsNatDocument();
-        this.itsNatHTMLDocument_ = (ItsNatHTMLDocument) itsNatDocument_;
-        this.hTMLDocument_ = itsNatHTMLDocument_.getHTMLDocument();
-        this.itsNatServlet_ = itsNatDocument_.getItsNatDocumentTemplate().getItsNatServlet();
-        this.itsNatHttpSession = (ItsNatHttpSession) request_.getItsNatSession();
-        this.location_ = (String) request_.getServletRequest().getAttribute("location");
-        init();
-        registerEventListeners();
+        boolean initializeFailed = true;
+        final StringBuilder log = new StringBuilder();
+        init:
+        {
+
+            this.itsNatDocument_ = request_.getItsNatDocument();
+            this.itsNatHTMLDocument_ = (ItsNatHTMLDocument) itsNatDocument_;
+            this.hTMLDocument_ = itsNatHTMLDocument_.getHTMLDocument();
+            this.itsNatServlet_ = itsNatDocument_.getItsNatDocumentTemplate().getItsNatServlet();
+            this.itsNatHttpSession = (ItsNatHttpSession) request_.getItsNatSession();
+            this.sBLoggedOnUserFace = (SBLoggedOnUserFace) itsNatHttpSession.getAttribute(ServletLogin.SBLoggedOnUser);
+            this.location_ = (String) request_.getServletRequest().getAttribute("location");
+            init();
+            registerEventListeners(itsNatHTMLDocument_, hTMLDocument_, itsNatDocument_);
+
+            /**
+             * break. Do not let this statement be reachable if initialization
+             * failed. Instead, break immediately where initialization failed.
+             * At this point, we set the initializeFailed to false and thereby,
+             * allow initialization of an instance
+             */
+            initializeFailed = false;
+        }
+        if (initializeFailed) {
+            throw new ExceptionConstructorInvokation(log.toString());
+        }
     }
 
     /**
@@ -49,8 +70,11 @@ public abstract class AbstractListener {
      * Use ItsNatHTMLDocument variable stored in the AbstractListener class
      * Do not call this method anywhere, just implement it, as it will be
      * automatically called by the contructor
+     * @param itsNatHTMLDocument_
+     * @param itsNatDocument__
+     * @param hTMLDocument_
      */
-    protected abstract void registerEventListeners();
+    protected abstract void registerEventListeners(final ItsNatHTMLDocument itsNatHTMLDocument_, final HTMLDocument hTMLDocument_, final ItsNatDocument itsNatDocument__);
 
     /**
      * In general, avoid registering the same template again to avoid element
@@ -76,7 +100,7 @@ public abstract class AbstractListener {
     /**
      * This Map is static as Id's in html documents should be universally identical, i.e. as htmldocname_elementId
      */
-    protected final static Map<String, String> GlobalHTMLIdRegistry_ = Controller.GlobalHTMLIdRegistry;//new IdentityHashMap<String, String>();
+    protected final static Map<String, String> GlobalHTMLIdRegistry_ = Controller.GlobalHTMLIdRegistry;
 
     /**
      * Id registry should be globally visible to callers
@@ -87,7 +111,7 @@ public abstract class AbstractListener {
     protected final Element getElementById(final String key__) {
         final String elementId__ = GlobalHTMLIdRegistry_.get(key__);
         if (elementId__ == null) {
-            throw new java.lang.NullPointerException("ELEMENT \"" + key__ + "\" CONTAINS NULL OR NO REFERENCE IN REGISTRY!");
+            throw new java.lang.NullPointerException("THE ELEMENT \"" + key__ + "\" YOU REQUIRE CONTAINS NULL OR NO REFERENCE IN REGISTRY!");
         }
         return hTMLDocument_.getElementById(elementId__);
     }
