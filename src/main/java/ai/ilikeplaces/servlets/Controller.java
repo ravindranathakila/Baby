d43373d9da9ec6829818763a97b8d2169750b6be
akila <akila>
2009-09-24 14:33:53 +0000

    "20090924 Changed enum type to be accessible via interface for flexibility\n"

--- src/main/java/ai/ilikeplaces/servlets/Controller.java
+++ src/main/java/ai/ilikeplaces/servlets/Controller.java
@@ -3,14 +3,12 @@ package ai.ilikeplaces.servlets;
 import ai.ilikeplaces.ListenerLogin;
 import ai.ilikeplaces.ListenerMain;
 import java.io.IOException;
-import java.util.Set;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.itsnat.core.ItsNatDocument;
 import org.itsnat.core.ItsNatServletConfig;
 import org.itsnat.core.ItsNatServletRequest;
@@ -37,41 +35,62 @@ public class Controller extends HttpServletWrapper {
     /**
      * Initializer for pages with their ids and paths
      */
-    public enum Page {
+    public enum Page implements PageFace {
 
-        home(""),
-        main("ai/ilikeplaces/Main.xhtml", "Main_temp1", "Main_temp2", "Main_sidebar"),
-        Photo$Description("ai/ilikeplaces/fragments/Photo-Description.xhtml", "pd"),
-        login("ai/ilikeplaces/security/login.xhtml"),
-        include("ai/ilikeplaces/security/include.xhtml");
+        home(null) {
+
+            @Override
+            public String toString() {
+                return "/ilikeplaces/page/main";
+            }
+        },
+        main("ai/ilikeplaces/Main.xhtml", "Main_temp1", "Main_temp2", "Main_sidebar") {
+
+            @Override
+            public String toString() {
+                return "main";
+            }
+        },
+        Photo$Description("ai/ilikeplaces/fragments/Photo-Description.xhtml", "pd") {
+
+            @Override
+            public String toString() {
+                return "Photo-Description";
+            }
+        },
+        PhotoUpload("ai/ilikeplaces/widgets/PhotoUpload.xhtml"){
+
+            @Override
+            public String toString() {
+                return "PhotoUpload";
+            }
+        },
+        signup(null) {
+
+            @Override
+            public String toString() {
+                return "/ilikeplaces/signup";
+            }
+        },
+        login("ai/ilikeplaces/security/login.xhtml") {
+
+            @Override
+            public String toString() {
+                return "login";
+            }
+        },
+        include("ai/ilikeplaces/security/include.xhtml") {
+
+            @Override
+            public String toString() {
+                return "include";
+            }
+        };
 
         private Page(final String path__, final String... ids__) {
             PrettyURLMap_.put(this, path__);
             PutAllPageElementIds(ids__);
-            PutAllPageElementIdsByPage(this,ids__);
-        }
-
-        @Override
-        public String toString() {
-            String returnVal = null;
-            switch (this) {
-                case home:
-                    returnVal = "/ilikeplaces/page/main";
-                    break;
-                case main:
-                    returnVal = "main";
-                    break;
-                case Photo$Description:
-                    returnVal = "Photo-Description";
-                    break;
-                case login:
-                    returnVal = "login";
-                    break;
-                case include:
-                    returnVal = "include";
-                    break;
-            }
-            return returnVal;
+            PutAllPageElementIdsByPage(this, ids__);
         }
     }
 
@@ -94,15 +113,14 @@ public class Controller extends HttpServletWrapper {
         /*Add a listner to convert pretty urls to proper urls*/
         inhs__.addItsNatServletRequestListener(new ItsNatServletRequestListener() {
 
+            @Override
             public void processRequest(final ItsNatServletRequest request__, final ItsNatServletResponse response__) {
                 final ItsNatDocument itsNatDocument__ = request__.getItsNatDocument();
                 /*if(itsNatDocument != null && ((HttpServletRequest) request__.getServletRequest()).getPathInfo().contains("itsnat_doc_name")){
                 throw new java.lang.RuntimeException("INVALID URL");//This code does not seem to work, please verify.
                 }*/
                 if (itsNatDocument__ == null && request__.getServletRequest().getAttribute("itsnat_doc_name") == null) {
-                    HttpServletRequest httpServletRequest = (HttpServletRequest) request__.getServletRequest();
-//                    final String pathInfo__ = httpServletRequest.getPathInfo();
-//                    request__.getServletRequest().setAttribute("itsnat_doc_name", pathInfo__.substring(1));
+                    final  HttpServletRequest httpServletRequest = (HttpServletRequest) request__.getServletRequest();
                     pathResolver(request__);
                     request__.getItsNatServlet().processRequest(httpServletRequest, response__.getServletResponse());
                 }
@@ -112,60 +130,24 @@ public class Controller extends HttpServletWrapper {
         final String realPath__ = getServletContext().getRealPath("/");
         final String pathPrefix__ = realPath__ + "WEB-INF/pages/";
 
-        inhs__.registerItsNatDocumentTemplate(Page.main.toString(), "text/html", pathPrefix__ + PrettyURLMap_.get(Page.main)).addItsNatServletRequestListener(new ListenerMain());
+        final PageFace main = Page.main;
 
-        inhs__.registerItsNatDocumentTemplate("login", "text/html", pathPrefix__ + PrettyURLMap_.get(Page.login)).addItsNatServletRequestListener(new ListenerLogin());
+        inhs__.registerItsNatDocumentTemplate(main.toString(), "text/html", pathPrefix__ + PrettyURLMap_.get(main)).addItsNatServletRequestListener(new ListenerMain());
 
-        inhs__.registerItsNatDocFragmentTemplate(Page.Photo$Description.toString(), "text/html", pathPrefix__ + PrettyURLMap_.get(Page.Photo$Description));
+        final PageFace photo$Description = Page.Photo$Description;
+        inhs__.registerItsNatDocFragmentTemplate(photo$Description.toString(), "text/html", pathPrefix__ + PrettyURLMap_.get(photo$Description));
 
+        final PageFace photoUpload = Page.PhotoUpload;
+        inhs__.registerItsNatDocFragmentTemplate(photoUpload.toString(), "text/html", pathPrefix__ + PrettyURLMap_.get(photoUpload));
 
-        inhs__.registerItsNatDocFragmentTemplate("include2", "text/html", pathPrefix__ + PrettyURLMap_.get(Page.include));
+        final PageFace login = Page.login;
+
+        inhs__.registerItsNatDocumentTemplate("login", "text/html", pathPrefix__ + PrettyURLMap_.get(login)).addItsNatServletRequestListener(new ListenerLogin());
+
+        final PageFace include = Page.include;
+        inhs__.registerItsNatDocFragmentTemplate("include2", "text/html", pathPrefix__ + PrettyURLMap_.get(include));
     }
 
-//    @Override
-//    public void doGet(HttpServletRequest request__, HttpServletResponse response__) throws ServletException, IOException{
-//
-//        final HttpSession UserSession = request__.getSession(false);
-//
-//        /**
-//         * We have two types of users.
-//         * > Users who do not wish to login to this site(just browse)
-//         * and
-//         * > who have an account.
-//         *      Users who have an account are either
-//         * >> Logged in
-//         * >> Logged out(normal or timeout)
-//         * Login is handled by the login servlet, which will set "User" parameter
-//         * to be true. i.e. this is a user which has an account.
-//         * That servlet will also add the relevant stateful session bean to the
-//         * users session.
-//         *
-//         * Now in this servlet,
-//         * 1. We have to let web users pass by without interfereing their
-//         * request.
-//         * 2. We have to make sure the "user" is logged in if not, by verifying
-//         * that the stateful session bean is present. If not, redirect him to the
-//         * login page.
-//         *
-//         */
-//        if(UserSession != null){/*Do not process a general user, proceed at the earliest (e.g. Refered from a search page*/
-//            /*Ok the httpsession is live, is this user logged in?*/
-//            if(UserSession.getAttribute(ServletLogin.User) != null){
-//                //This is a user.
-//                //Put so in the request and itsnathttpsession too.
-//                //do all this in the sign in servlet, except itsnathttpsession
-//
-//            } else {/*Web user who somehow had a session object*/
-//                super.doGet(request__, response__);
-//            }
-//        } else {/*Ok user came directly to pages, he does not want to login, and did not visit the login page*/
-//            super.doGet(request__, response__);
-//        }
-//    }
-    @Override
-    public void doPost(HttpServletRequest request__, HttpServletResponse response__) throws ServletException, IOException{
-        super.doPost(request__, response__);
-    }
     /**
      * We have a URL of type say www.ilikeplaces.com/page/Egypt
      * where we are supposed to recieve the /Egypt part.
@@ -213,20 +195,19 @@ public class Controller extends HttpServletWrapper {
             }
         }
     }
-    
     /**
      * Retrievable list of all element Ids by Page
      */
-    public final static Map<Page, HashSet<String>>  GlobalPageIdRegistry = new IdentityHashMap<Page, HashSet<String>>();
+    public final static Map<PageFace, HashSet<String>> GlobalPageIdRegistry = new IdentityHashMap<PageFace, HashSet<String>>();
 
-    private final static void PutAllPageElementIdsByPage(final Page page__, final String... ids__) {
+    private final static void PutAllPageElementIdsByPage(final PageFace page__, final String... ids__) {
         final HashSet<String> ids_ = new HashSet<String>();
         for (final String id_ : ids__) {
-                /**
-                 * Do not verify if element exists in "document" here as elements
-                 * can be dynamically created
-                 */
-                ids_.add(id_);
+            /**
+             * Do not verify if element exists in "document" here as elements
+             * can be dynamically created
+             */
+            ids_.add(id_);
         }
         GlobalPageIdRegistry.put(page__, ids_);
     }
@@ -238,7 +219,8 @@ public class Controller extends HttpServletWrapper {
      */
     public String toString(final boolean showChangeLog__) {
         String changeLog = new String(toString() + "\n");
-        changeLog += "20090918 Inluded home link in the enum, to be used by everybody. \n";
+        changeLog += "20090918 Addedhome link in the enum, to be used by everybody. \n";
+        changeLog += "20090924 Changed enum type to be accessible via interface for flexibility\n";
         return showChangeLog__ ? changeLog : toString();
     }
 }
