4cdb3150ca66df9feddfc18e57c1986a4aa605d3
vergy <vergy>
2009-10-03 08:37:53 +0000

    20091003 new  FIXME and TODO

---
+++ src/main/java/ai/ilikeplaces/doc/TODO.java
@@ -0,0 +1,13 @@
+package ai.ilikeplaces.doc;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+/**
+ *
+ * @author Ravindranath Akila
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+public @interface TODO {
+    String[] tasks();
+}
