333bf34087d678bbfca8364f4136797529b030bc
vergy <vergy>
2009-10-04 04:26:50 +0000

    2009104 new

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/MarkupTagFace.java
@@ -0,0 +1,15 @@
+package ai.ilikeplaces.logic.Listeners;
+
+/**
+ *
+ * @author Ravindranath Akila
+ */
+public interface MarkupTagFace {
+
+    /**
+     * Exact String Representation of Enum.
+     * Case sensitive as XML markup could be added here in future
+     * @return
+     */
+    public String toString();
+}
