36fd1da3ed55aaec51a9543a18885bc75d2541c8
vergy <vergy>
2009-10-14 07:16:00 +0000

    20091014 new

---
+++ src/main/java/ai/ilikeplaces/logic/crud/CHuman.java
@@ -0,0 +1,24 @@
+package ai.ilikeplaces.logic.crud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ai.ilikeplaces.doc.*;
+import ai.ilikeplaces.util.AbstractSLBCallbacks;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+/**
+ *
+ * @author Ravindranath Akila
+ */
+@License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+public class CHuman extends AbstractSLBCallbacks {
+
+    public CHuman() {
+        logger.debug("HELLO, I INSTANTIATED {} OF WHICH HASHCODE IS {}.", CHuman.class, this.hashCode());
+    }
+    final static Logger logger = LoggerFactory.getLogger(CHuman.class);
+}
