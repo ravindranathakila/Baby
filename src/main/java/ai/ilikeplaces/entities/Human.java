1a9a52460982eb215542172cb23cca943e564814
vergy <vergy>
2010-01-12 16:20:29 +0000

    toString. commente-gender code removal. pkinterface.

--- src/main/java/ai/ilikeplaces/entities/Human.java
+++ src/main/java/ai/ilikeplaces/entities/Human.java
@@ -1,28 +1,19 @@
 package ai.ilikeplaces.entities;
 
-import ai.ilikeplaces.util.*;
+import ai.ilikeplaces.util.EntityLifeCyleListener;
+
+import javax.persistence.*;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import org.slf4j.LoggerFactory;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 /**
- *
  * @author Ravindranath Akila
  */
 @Entity
 @EntityListeners(EntityLifeCyleListener.class)
-public class Human implements Serializable {
+public class Human implements HumanIdFace, Serializable {
 
-//    public static final Integer genderCodeNeutral = 0;
-//    public static final Integer genderCodeMale = 1;
-//    public static final Integer genderCodeFemale = 2;
     private String humanId;
+
     /**
      * If the user is not active on the site, he is considered dead and the
      * account shall be false under humanAlive.
@@ -92,7 +83,7 @@ public class Human implements Serializable {
     public void setHumansPublicPhoto(HumansPublicPhoto humansPublicPhoto) {
         this.humansPublicPhoto = humansPublicPhoto;
     }
-    
+
     @OneToOne(cascade = CascadeType.ALL)
     @PrimaryKeyJoinColumn
     public HumansNet getHumansNet() {
@@ -103,43 +94,12 @@ public class Human implements Serializable {
         this.humansNet = humansNet;
     }
 
-    /**
-     *
-     * @return toString_
-     */
+
     @Override
     public String toString() {
-        String toString_ = new String(getClass().getName());
-        try {
-            final Field[] fields = {getClass().getDeclaredField("humanId")};
-
-            for (final Field field : fields) {
-                try {
-                    toString_ += "\n{" + field.getName() + "," + field.get(this) + "}";
-                } catch (IllegalArgumentException ex) {
-                    LoggerFactory.getLogger(Location.class.getName()).error( null, ex);
-                } catch (IllegalAccessException ex) {
-                    LoggerFactory.getLogger(Location.class.getName()).error( null, ex);
-                }
-            }
-        } catch (NoSuchFieldException ex) {
-            LoggerFactory.getLogger(Location.class.getName()).error( null, ex);
-        } catch (SecurityException ex) {
-            LoggerFactory.getLogger(Location.class.getName()).error( null, ex);
-        }
-
-        return toString_;
-    }
-
-    /**
-     *
-     * @param showChangeLog__
-     * @return changeLog
-     */
-    public String toString(final boolean showChangeLog__) {
-        String changeLog = new String(toString() + "\n");
-        changeLog += "20090914 Initial \n";
-        changeLog += "20090918 Added enum for user gender and many other variables \n";
-        return showChangeLog__ ? changeLog : toString();
+        return "Human{" +
+                "humanId='" + humanId + '\'' +
+                ", humanAlive=" + humanAlive +
+                '}';
     }
 }
