964ee1d710b51c033e4d5b5b402fe8b69c562f11
vergy <vergy>
2010-02-10 09:42:56 +0000

    new

---
+++ src/main/site/diagrams/i_like_places_user.mm
@@ -0,0 +1,274 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1265820657562" ID="Freemind_Link_928670984" MODIFIED="1265831127609" STYLE="bubble" TEXT="What Is I Like Place?&#xa;What Can I Do Here?">
+<edge COLOR="#009999" WIDTH="2"/>
+<font BOLD="true" NAME="Comic Sans MS" SIZE="20"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>This mind map should help you&#xa;figure out the purpose of the site.&#xa;You should also be able to:&#xa;1. Find out how you can use it&#xa;2. Find out how you can work with us&#xa;3. Help out fellow humans to enjoy and have fun</text>
+</hook>
+<node COLOR="#0033ff" CREATED="1265820689703" ID="_" MODIFIED="1265828339250" POSITION="right" STYLE="bubble" TEXT="Search &#xa;For A Location">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265820723218" ID="Freemind_Link_1720051334" MODIFIED="1265828339250" STYLE="fork" TEXT="Why &#xa;Search For A Location?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265820746125" ID="Freemind_Link_557455402" MODIFIED="1265829661218" STYLE="bubble" TEXT="To Organize An Event There">
+<arrowlink DESTINATION="Freemind_Link_415175333" ENDARROW="Default" ENDINCLINATION="1513;0;" ID="Freemind_Arrow_Link_853206187" STARTARROW="None" STARTINCLINATION="1513;0;"/>
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1265830159781" ID="Freemind_Link_1377226182" MODIFIED="1265830183750" STYLE="fork" TEXT="How To &#xa;Search For A Location">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265830194718" ID="Freemind_Link_245868329" MODIFIED="1265830427531" STYLE="bubble" TEXT="Try It In The Address Bar. &#xa;For Paris,&#xa;http://www.ilikeplaces.com/page/Paris">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Proper names including proper capitalization in the URL will get you to the location. &#xa;Wrong capitalization will show you the possible results you were looking for. &#xa;Then you can click on the proper location name you were looking for.</text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265830434125" ID="Freemind_Link_455822703" MODIFIED="1265830547140" STYLE="bubble" TEXT="Try Searching It. &#xa;The results page will show most probable locations &#xa;you would be interested in.">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Typing in &quot;paris&quot; will show you there result &quot;Paris&quot; and any other locations starting with the letters &quot;paris&quot;.</text>
+</hook>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1265820767109" HGAP="23" ID="Freemind_Link_809209352" MODIFIED="1265828339234" POSITION="right" STYLE="bubble" TEXT="Find Out &#xa;Good Places To Visit" VSHIFT="93">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265820795828" ID="Freemind_Link_1390735058" MODIFIED="1265828339234" STYLE="fork" TEXT="How To &#xa;Find Good Places To Visit?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265820809375" ID="Freemind_Link_209330475" MODIFIED="1265828339234" STYLE="bubble" TEXT="Find &#xa;Where Friends Visited And Recommend">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265820957750" ID="Freemind_Link_585751874" MODIFIED="1265828339218" STYLE="fork" TEXT="How To &#xa;Find Where Friends Visited And Recommended?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265820975671" ID="Freemind_Link_1496576387" MODIFIED="1265830612203" STYLE="bubble" TEXT="Check Wall Updates">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265820982531" ID="Freemind_Link_1179677403" MODIFIED="1265830592531" STYLE="bubble" TEXT="Follow Friends">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265820986203" ID="Freemind_Link_475114137" MODIFIED="1265830616843" STYLE="bubble" TEXT="Check Album Updates">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1265820833671" ID="Freemind_Link_852905387" MODIFIED="1265828339218" STYLE="bubble" TEXT="Find &#xa;Where Users Ranked">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265820868953" ID="Freemind_Link_1441914513" MODIFIED="1265828339218" STYLE="fork" TEXT="How To &#xa;Find Where Users Ranked?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265820877281" ID="Freemind_Link_72684897" MODIFIED="1265828339203" STYLE="bubble" TEXT="Top Ranked List">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265820930078" ID="Freemind_Link_1440976873" MODIFIED="1265828339203" STYLE="bubble" TEXT="Search Sub Locations">
+<arrowlink DESTINATION="_" ENDARROW="Default" ENDINCLINATION="805;0;" ID="Freemind_Arrow_Link_948236940" STARTARROW="None" STARTINCLINATION="805;0;"/>
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1265824545578" ID="Freemind_Link_438652836" MODIFIED="1265828339203" POSITION="left" TEXT="Socialize In Real World">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265824678843" ID="Freemind_Link_1691934971" MODIFIED="1265828339203" STYLE="fork" TEXT="How &#xa;Do I Do That?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265824726281" ID="Freemind_Link_999819129" MODIFIED="1265828339203" STYLE="bubble" TEXT="Organize Events With Friends Where You Can Socialize">
+<arrowlink DESTINATION="Freemind_Link_1722935537" ENDARROW="Default" ENDINCLINATION="1262;0;" ID="Freemind_Arrow_Link_1227951803" STARTARROW="None" STARTINCLINATION="1262;0;"/>
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1265822431265" HGAP="29" ID="Freemind_Link_1861244999" MODIFIED="1265828339187" POSITION="left" STYLE="bubble" TEXT="Create &#xa;Private Locations" VSHIFT="43">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265822447750" HGAP="40" ID="Freemind_Link_1169012472" MODIFIED="1265828339187" STYLE="fork" TEXT="Why &#xa;Should I Create Private Locations?" VSHIFT="-68">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265822456406" ID="Freemind_Link_574890459" MODIFIED="1265828339187" STYLE="bubble" TEXT="To &#xa;Create Private Events In Them">
+<arrowlink DESTINATION="Freemind_Link_1722935537" ENDARROW="Default" ENDINCLINATION="1184;0;" ID="Freemind_Arrow_Link_612955048" STARTARROW="None" STARTINCLINATION="1184;0;"/>
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265823092578" ID="Freemind_Link_1040740812" MODIFIED="1265828339171" STYLE="fork" TEXT="Why &#xa;Create Private Events Here?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265823102234" ID="Freemind_Link_663205890" MODIFIED="1265828339171" STYLE="bubble" TEXT="To &#xa;Invite Friends">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265823115546" ID="Freemind_Link_1898238561" MODIFIED="1265828339156" STYLE="bubble" TEXT="To &#xa;Manage Necessary Stuff For The Event">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265823181187" ID="Freemind_Link_590538994" MODIFIED="1265828339156" STYLE="fork" TEXT="What &#xa;Necessary Stuff?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265823187093" ID="Freemind_Link_558003866" MODIFIED="1265828339140" STYLE="bubble" TEXT="Food And Beverages">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265823196546" ID="Freemind_Link_943282084" MODIFIED="1265828339140" STYLE="bubble" TEXT="Stuff Which Friends Should Bring">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265823212156" ID="Freemind_Link_294481918" MODIFIED="1265828339140" STYLE="bubble" TEXT="Gear, If The Location Needs Travelling">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265823232156" ID="Freemind_Link_621219413" MODIFIED="1265828339140" STYLE="bubble" TEXT="Etc.">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1265822479406" ID="Freemind_Link_633034933" MODIFIED="1265828339109" STYLE="bubble" TEXT="To &#xa;Maintain A Long Term List Of Events Held There">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265822523734" ID="Freemind_Link_332006861" MODIFIED="1265828339109" STYLE="fork" TEXT="Why &#xa;Maintain This List?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265822667031" ID="Freemind_Link_319024334" MODIFIED="1265828339093" STYLE="bubble" TEXT="Say You Have Had 5 Birthdays Here.&#xa;Say You Had 10 Parties Here. &#xa;All These Events Can Be Seen.">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1265822531953" ID="Freemind_Link_1454528888" MODIFIED="1265828339093" STYLE="bubble" TEXT="All Your Albums Can Be Maintained Here">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1265821475421" ID="Freemind_Link_1722935537" MODIFIED="1265828339078" POSITION="left" STYLE="bubble" TEXT="Organize &#xa;Events in Places" VSHIFT="73">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265821573609" ID="Freemind_Link_1121862" MODIFIED="1265828339062" STYLE="fork" TEXT="Where &#xa;Can I Organize Events?">
+<arrowlink DESTINATION="Freemind_Link_1416123445" ENDARROW="Default" ENDINCLINATION="55;0;" ID="Freemind_Arrow_Link_1277072609" STARTARROW="None" STARTINCLINATION="55;0;"/>
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265821281718" ID="Freemind_Link_1038641817" MODIFIED="1265828339062" STYLE="bubble" TEXT="Plan Events in Public Places">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265821251968" ID="Freemind_Link_670488801" MODIFIED="1265828339062" STYLE="bubble" TEXT="Plan Events in Private Places">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1265821602234" ID="Freemind_Link_1416123445" MODIFIED="1265828339046" STYLE="fork" TEXT="How &#xa;Can I Organize Events?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265821428687" ID="Freemind_Link_40406020" MODIFIED="1265828339046" STYLE="bubble" TEXT="Go to Public Location">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265822038171" ID="Freemind_Link_1466269949" MODIFIED="1265828339046" STYLE="fork" TEXT="Then?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265822121781" ID="Freemind_Link_1634031475" MODIFIED="1265828339046" STYLE="bubble" TEXT="Create A Private Event">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1265822363125" ID="Freemind_Link_886387581" MODIFIED="1265828339046" STYLE="bubble" TEXT="Go to Private Location">
+<arrowlink DESTINATION="Freemind_Link_1861244999" ENDARROW="Default" ENDINCLINATION="445;0;" ID="Freemind_Arrow_Link_323372699" STARTARROW="None" STARTINCLINATION="445;0;"/>
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265821372437" ID="Freemind_Link_681791106" MODIFIED="1265828339046" STYLE="fork" TEXT="Then?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265822190671" ID="Freemind_Link_653123814" MODIFIED="1265828339046" STYLE="bubble" TEXT="Create A Private Event">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1265821665296" ID="Freemind_Link_1482322711" MODIFIED="1265828339031" STYLE="fork" TEXT="What &#xa;Kind Of Places Are Available?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265821683500" ID="Freemind_Link_1947009042" MODIFIED="1265828339015" STYLE="bubble" TEXT="Public Locations">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265821863296" ID="Freemind_Link_1583002122" MODIFIED="1265828339015" STYLE="fork" TEXT="What is a Public Location?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265821870687" ID="Freemind_Link_494517997" MODIFIED="1265828339015" STYLE="bubble" TEXT="A Location Where Any Person Can Visit">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1265821693515" ID="Freemind_Link_876405697" MODIFIED="1265828339015" STYLE="bubble" TEXT="Private Locations">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265821907906" ID="Freemind_Link_281915187" MODIFIED="1265828339015" STYLE="fork" TEXT="What is a Private Location?">
+<font NAME="Comic Sans MS" SIZE="12"/>
+<node COLOR="#111111" CREATED="1265821915187" ID="Freemind_Link_1561861538" MODIFIED="1265828338953" STYLE="bubble" TEXT="A Location Where Only People You Allow Can Visit">
+<font NAME="Comic Sans MS" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1265828561500" ID="Freemind_Link_415175333" MODIFIED="1265829661218" STYLE="fork" TEXT="What  Kind Of Events Can I Organize?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265828629968" ID="Freemind_Link_337932262" MODIFIED="1265829656859" STYLE="bubble" TEXT="Birthday Parties">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265828655390" ID="Freemind_Link_255273705" MODIFIED="1265829656875" STYLE="bubble" TEXT="Anniversaries">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265828662078" ID="Freemind_Link_1627908969" MODIFIED="1265830072187" STYLE="bubble" TEXT="Gaming Nights">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;The Super Bowl night at your place with your friends. </text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265828668312" ID="Freemind_Link_1872620825" MODIFIED="1265829636171" STYLE="bubble" TEXT="Proms">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265828675078" ID="Freemind_Link_516037867" MODIFIED="1265829636203" STYLE="bubble" TEXT="Weddings">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265828680750" ID="Freemind_Link_375178316" MODIFIED="1265830066562" STYLE="bubble" TEXT="Holiday Trips">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Trip to Hawai.&#xa;Trip to your museam. </text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265828687250" ID="Freemind_Link_1722074502" MODIFIED="1265830052734" STYLE="bubble" TEXT="Relaxation Vacations">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Vacation at a Meditaion Center. &#xa;Vacation at a Ayurvedic and Yoga Healing Center. </text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265829524015" ID="Freemind_Link_1691413378" MODIFIED="1265830059062" STYLE="bubble" TEXT="Charityable Events">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Collecting donations to help a patient at the main shopping mall of your town. </text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265829538015" ID="Freemind_Link_916549278" MODIFIED="1265830057171" STYLE="bubble" TEXT="Campaigns">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Run campaigns relevant to your interests at places you choose. </text>
+</hook>
+</node>
+<node COLOR="#990000" CREATED="1265829573921" ID="Freemind_Link_1487150181" MODIFIED="1265830055375" STYLE="bubble" TEXT="Community Service Events">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>e.g.&#xa;Cleaning up the area. &#xa;Helping out the poor in constructing their houses. </text>
+</hook>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1265828962218" ID="Freemind_Link_1769374872" MODIFIED="1265829175656" POSITION="right" STYLE="bubble" TEXT="Help Others">
+<font NAME="Comic Sans MS" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1265828992281" ID="Freemind_Link_667990858" MODIFIED="1265829011781" STYLE="fork" TEXT="How&#xa;Can I Help?">
+<font NAME="Comic Sans MS" SIZE="16"/>
+<node COLOR="#990000" CREATED="1265829013203" ID="Freemind_Link_331638355" MODIFIED="1265829033843" STYLE="bubble" TEXT="Help Friends Socialize">
+<font NAME="Comic Sans MS" SIZE="14"/>
+<node COLOR="#111111" CREATED="1265829034843" ID="Freemind_Link_1057659684" MODIFIED="1265829045671" STYLE="fork" TEXT="Why Help Friends Socialize?">
+<node COLOR="#111111" CREATED="1265829046125" ID="Freemind_Link_763970314" MODIFIED="1265829064250" STYLE="bubble" TEXT="So That You Will Have Friends To Socialize With"/>
+<node COLOR="#111111" CREATED="1265829065265" ID="Freemind_Link_1310341374" MODIFIED="1265829118531" STYLE="bubble" TEXT="So That You and They Don&apos;t Socialize On The Internet, But Socialize In The Real World."/>
+<node COLOR="#111111" CREATED="1265829092500" ID="Freemind_Link_618450836" MODIFIED="1265829197671" STYLE="bubble" TEXT="So That You And They Be More Probable To Socialize">
+<node COLOR="#111111" CREATED="1265829214500" ID="Freemind_Link_1335084951" MODIFIED="1265829219109" STYLE="fork" TEXT="Why Socialize?">
+<node COLOR="#111111" CREATED="1265829219984" ID="Freemind_Link_827469340" MODIFIED="1265831073750" STYLE="bubble" TEXT="We Are Not Really Eager To Explain This To You.&#xa;Unless You Want To Spend A Really Boring Life,&#xa;And Believe In Happiness Only After Death,&#xa;We Strongly Believe You Try Being Happy Here On Earth,&#xa;Is Important.&#xa;Unless You Believe So Too,&#xa;Please Avoid Using This Site:-)">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Quoting &quot;Three Muskateers&quot;... &#xa;ARAMIS: We&apos;ll see you in Calais!... &#xa;ATHOS: Or hell!...</text>
+</hook>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1265831266171" ID="Freemind_Link_1852730896" MODIFIED="1265831273968" STYLE="bubble" TEXT="Help With Development">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265831274656" ID="Freemind_Link_602027469" MODIFIED="1265831281500" STYLE="bubble" TEXT="Help With Site Security">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265831282093" ID="Freemind_Link_547286573" MODIFIED="1265831287328" STYLE="bubble" TEXT="Help With Feedback">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265831289734" ID="Freemind_Link_1503149777" MODIFIED="1265831297921" STYLE="bubble" TEXT="Help With Blogging About Us">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265831299578" ID="Freemind_Link_1108885257" MODIFIED="1265831306312" STYLE="bubble" TEXT="Help With Sharing Us">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1265831307156" ID="Freemind_Link_526031956" MODIFIED="1265831312062" STYLE="bubble" TEXT="Help With Recommending Us">
+<font NAME="Comic Sans MS" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
