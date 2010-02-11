b401e54c856803e1dc9d56fba674ec81cfeb4d20
vergy <vergy>
2010-02-11 06:30:25 +0000

    cosmetic

--- src/main/java/ai/ilikeplaces/logic/crud/unit/UPrivateLocationLocal.java
+++ src/main/java/ai/ilikeplaces/logic/crud/unit/UPrivateLocationLocal.java
@@ -1,5 +1,6 @@
 package ai.ilikeplaces.logic.crud.unit;
 
+import ai.ilikeplaces.doc.License;
 import ai.ilikeplaces.entities.PrivateLocation;
 
 import javax.ejb.Local;
@@ -12,7 +13,7 @@ import java.util.List;
  * Time: 10:31:21 PM
  */
 
-// @License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
+@License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
 @Local
 public interface UPrivateLocationLocal {
 
