0307fb81cb427d8abb74146afc0666b8c14a7db5
vergy <vergy>
2010-11-13 05:26:10 +0000

    freezing

---
+++ src/main/site/Now What.mm
@@ -0,0 +1,86 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#00cc66" CREATED="1289573930062" ID="Freemind_Link_1993118448" MODIFIED="1289625920625" TEXT="I&apos;m in, &#xa;now what?">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Use Jackie Chan English. Makes understand concept easy. Hokay?&#xa;This should show all use cases.&#xa;Use pull marketing. Makes marketing easy. What is pull marketing? Market only something wanted. If they don&apos;t want, try make them want. Simple. Hokay?&#xa;To use pull marketing in diagram, use want/get to grow nodes. Make wants nodes(give more importance). Make gets chains. Gives can have &quot;get&quot; or &quot;notice&quot; as the explanatory word. Nothing else.&#xa;Remember, if a want gives u a headache hor disturb, then that is the good want. Needs fixing. Hokay? Pay more attention this wants.&#xa;Remember that not all nodes are marketable. Some simplify things but not directly marketable. Instead, prevents stagnation.&#xa;&#xa;get gone = browser sends/application sends</text>
+</hook>
+<node CREATED="1289573987921" ID="_" MODIFIED="1289573987921" POSITION="right" TEXT="">
+<node CREATED="1289573988109" ID="Freemind_Link_1687042404" MODIFIED="1289574103718" TEXT="I go home page">
+<node CREATED="1289574124406" FOLDED="true" ID="Freemind_Link_292432303" MODIFIED="1289620430109" STYLE="bubble" TEXT="I see search box, &#xa;i what to do something, do i?&#xa; if so what do i do?">
+<node CREATED="1289620321312" ID="Freemind_Link_1754743000" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+<node CREATED="1289620321312" ID="Freemind_Link_1951124893" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+</node>
+<node CREATED="1289574129750" ID="Freemind_Link_1537963368" MODIFIED="1289620458750" STYLE="bubble" TEXT="I see find button, &#xa;i what to do something, do i?&#xa; if so what do i do?">
+<node CREATED="1289620321312" ID="Freemind_Link_321153483" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+<node CREATED="1289620321312" ID="Freemind_Link_651565119" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+</node>
+<node CREATED="1289574115421" FOLDED="true" ID="Freemind_Link_1238654914" MODIFIED="1289621079015" STYLE="bubble" TEXT="i see map,&#xa;i what to do something, do i?&#xa; if so what do i do?">
+<node CREATED="1289620321312" FOLDED="true" ID="Freemind_Link_1677979916" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+<node CREATED="1289620321312" FOLDED="true" ID="Freemind_Link_1328873307" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+</node>
+<node CREATED="1289574141734" ID="Freemind_Link_1397007338" MODIFIED="1289620483906" STYLE="bubble" TEXT="I see big label, says Down Town, &#xa;i what to do something, do i?&#xa; if so what do i do?">
+<node CREATED="1289620321312" ID="Freemind_Link_899282387" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+<node CREATED="1289620321312" ID="Freemind_Link_1136022597" MODIFIED="1289620321312" STYLE="fork" TEXT=""/>
+</node>
+</node>
+</node>
+<node CREATED="1289574105546" ID="Freemind_Link_1444820136" MODIFIED="1289574109921" POSITION="left" TEXT="I go downtown page"/>
+<node CREATED="1289620553078" ID="Freemind_Link_1854861962" MODIFIED="1289625550046" POSITION="right" TEXT="I get organize page">
+<node CREATED="1289620566687" HGAP="59" ID="Freemind_Link_1867038270" MODIFIED="1289624802328" STYLE="bubble" TEXT="I see create location,&#xa; i want to create a location, do i?" VSHIFT="47">
+<node CREATED="1289620618031" ID="Freemind_Link_1995229372" MODIFIED="1289623924203" STYLE="fork" TEXT="I get a page with location i created">
+<node CREATED="1289620829593" ID="Freemind_Link_451903727" MODIFIED="1289621451890" STYLE="bubble" TEXT="I see create event,&#xa;I want to create event">
+<node CREATED="1289620830562" ID="Freemind_Link_154600273" MODIFIED="1289623944921" STYLE="fork" TEXT="I get a page with event i created">
+<node CREATED="1289624847828" ID="Freemind_Link_902365644" MODIFIED="1289624864078" STYLE="bubble" TEXT="I see invite section, I decide I need to invite people">
+<node CREATED="1289624934281" ID="Freemind_Link_1990382969" MODIFIED="1289624939015" STYLE="fork" TEXT="I focus on that section">
+<node CREATED="1289623945750" ID="Freemind_Link_268424223" MODIFIED="1289624413937" STYLE="bubble" TEXT="I see invite owners, I invite owners">
+<arrowlink COLOR="#006666" DESTINATION="Freemind_Link_1579652225" ENDARROW="Default" ENDINCLINATION="816;0;" ID="Freemind_Arrow_Link_973007867" STARTARROW="None" STARTINCLINATION="816;0;"/>
+<node CREATED="1289624434656" ID="Freemind_Link_709306926" MODIFIED="1289624436484" STYLE="fork" TEXT="I get acknowledged from my friend he became an owner"/>
+</node>
+<node CREATED="1289623945750" ID="Freemind_Link_1484935348" MODIFIED="1289624419062" STYLE="bubble" TEXT="I see invite viewers, I invite viewers">
+<arrowlink COLOR="#006666" DESTINATION="Freemind_Link_1606832476" ENDARROW="Default" ENDINCLINATION="820;0;" ID="Freemind_Arrow_Link_221268283" STARTARROW="None" STARTINCLINATION="820;0;"/>
+<node CREATED="1289624438078" ID="Freemind_Link_975825216" MODIFIED="1289624452968" STYLE="fork" TEXT="I get acknowledged from my friend he became a viewer"/>
+</node>
+<node CREATED="1289623945750" ID="Freemind_Link_48170508" MODIFIED="1289624423562" STYLE="bubble" TEXT="I see invite invites, I invite invites">
+<arrowlink COLOR="#006666" DESTINATION="Freemind_Link_973966629" ENDARROW="Default" ENDINCLINATION="816;0;" ID="Freemind_Arrow_Link_999530576" STARTARROW="None" STARTINCLINATION="816;0;"/>
+<node CREATED="1289624440375" ID="Freemind_Link_1335842491" MODIFIED="1289624457312" STYLE="fork" TEXT="I get acknowledged from my friend he became an invite"/>
+</node>
+</node>
+</node>
+<node CREATED="1289624946687" ID="Freemind_Link_1384644388" MODIFIED="1289625156078" STYLE="bubble" TEXT="I see a an input box, i guess its a wall. I want to post somethign in it.">
+<arrowlink COLOR="#ccccff" DESTINATION="Freemind_Link_1266073223" ENDARROW="Default" ENDINCLINATION="965;0;" ID="Freemind_Arrow_Link_1091304069" STARTARROW="None" STARTINCLINATION="965;0;"/>
+<node CREATED="1289625041015" ID="Freemind_Link_963002939" MODIFIED="1289625059593" STYLE="fork" TEXT="I get acknowledged by MEMBERS ONLY that they got the wall update"/>
+</node>
+</node>
+</node>
+<node CREATED="1289622737046" ID="Freemind_Link_884603631" MODIFIED="1289624397828" STYLE="bubble" TEXT="I see invite owners,&#xa;I invite owners">
+<arrowlink COLOR="#00cccc" DESTINATION="Freemind_Link_1123408580" ENDARROW="Default" ENDINCLINATION="335;0;" ID="Freemind_Arrow_Link_1974839201" STARTARROW="None" STARTINCLINATION="335;0;"/>
+<node CREATED="1289623955312" ID="Freemind_Link_1813190569" MODIFIED="1289624343625" STYLE="fork" TEXT="I get acknowledged from my friend he became an owner"/>
+</node>
+<node CREATED="1289622743359" ID="Freemind_Link_1118361603" MODIFIED="1289624403125" STYLE="bubble" TEXT="I see viewers,&#xa;I invite viewers">
+<arrowlink COLOR="#00cccc" DESTINATION="Freemind_Link_924141894" ENDARROW="Default" ENDINCLINATION="311;0;" ID="Freemind_Arrow_Link_523599527" STARTARROW="None" STARTINCLINATION="311;0;"/>
+<node CREATED="1289623984359" ID="Freemind_Link_427240956" MODIFIED="1289624346015" STYLE="fork" TEXT="I get acknowledged from my friend he became an viewer"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1289622687062" ID="Freemind_Link_618787317" MODIFIED="1289625555953" POSITION="right" TEXT="I get check email">
+<node CREATED="1289624020718" ID="Freemind_Link_1123408580" MODIFIED="1289625429203" STYLE="bubble" TEXT="I see an email that I was made an owner of a location, I want to know what this is all about"/>
+<node CREATED="1289624020718" ID="Freemind_Link_924141894" MODIFIED="1289625436156" STYLE="bubble" TEXT="I see an email that I was made an viewr of a location, I want to know what this is all about"/>
+<node CREATED="1289624034343" ID="Freemind_Link_1579652225" MODIFIED="1289625448640" STYLE="bubble" TEXT="I see an email that I was made an owner of an event, I want to know what this is all about"/>
+<node CREATED="1289624034343" ID="Freemind_Link_1606832476" MODIFIED="1289625452343" STYLE="bubble" TEXT="I see an email that I was made an viewer of an event, I want to know what this is all about"/>
+<node CREATED="1289624034343" ID="Freemind_Link_973966629" MODIFIED="1289625454875" STYLE="bubble" TEXT="I see an email that I was made an invite of an even, I want to know what this is all about"/>
+<node CREATED="1289625071437" ID="Freemind_Link_1266073223" MODIFIED="1289625241812" STYLE="bubble" TEXT="I see an email with entire wall of an event I&apos;m a member of,&#xa;I want to start writing on that wall&#xa;I want to get rid of this wall, I hate this event and its spammy">
+<node CREATED="1289625245328" ID="Freemind_Link_1198134216" MODIFIED="1289625259781" TEXT="I want to start writing on that wall">
+<node CREATED="1289625273781" ID="Freemind_Link_1832300428" MODIFIED="1289625306343" STYLE="fork" TEXT="I notice an input box to post to wall"/>
+</node>
+<node CREATED="1289625249984" ID="Freemind_Link_471227886" MODIFIED="1289625266984" TEXT="I want to get rid of this wall, I hate this event and its spammy">
+<node CREATED="1289625329968" ID="Freemind_Link_1062949566" MODIFIED="1289625742015" STYLE="fork" TEXT="I notice an unsubscribe link">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Is unsubscrbe from event, from wall, or from all event notifications. Hmmmm... wall is best. </text>
+</hook>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
