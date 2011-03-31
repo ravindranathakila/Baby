77432e5af2fe709373ed4489e2ee0f1ba8d8215a
vergy <vergy>
2011-03-31 14:26:59 +0000

    Initial Commit of Public Relations

---
+++ src/main/site/Public Relations.mm
@@ -0,0 +1,39 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1301352876218" ID="Freemind_Link_672074783" MODIFIED="1301352898671" TEXT="Public Relations">
+<node CREATED="1301352960937" ID="_" MODIFIED="1301352966171" POSITION="right" TEXT="Buzzwords"/>
+<node CREATED="1301353037421" ID="Freemind_Link_275689162" MODIFIED="1301353040546" POSITION="left" TEXT="Lobby Groups"/>
+<node CREATED="1301353049562" ID="Freemind_Link_441191675" MODIFIED="1301353052562" POSITION="right" TEXT="Spin"/>
+<node CREATED="1301353103375" ID="Freemind_Link_48629558" MODIFIED="1301353108562" POSITION="left" TEXT="Defining the Opponent"/>
+<node CREATED="1301353329265" ID="Freemind_Link_1582587341" MODIFIED="1301353331406" POSITION="right" TEXT="Methods">
+<node CREATED="1301353331781" ID="Freemind_Link_1259373399" MODIFIED="1301353334671" TEXT="Direct Mail"/>
+<node CREATED="1301353335203" ID="Freemind_Link_1606463667" MODIFIED="1301353337578" TEXT="Robocalling"/>
+<node CREATED="1301353338453" ID="Freemind_Link_244335059" MODIFIED="1301353342531" TEXT="Social Media"/>
+<node CREATED="1301353507781" ID="Freemind_Link_115881861" MODIFIED="1301353510546" TEXT="Subscriptions">
+<node CREATED="1301353510984" ID="Freemind_Link_1513276459" MODIFIED="1301353517390" TEXT="People rarely unsubscribe"/>
+<node CREATED="1301353517921" ID="Freemind_Link_1813544111" MODIFIED="1301353532796" TEXT="People tend to back their initial stand"/>
+<node CREATED="1301353534171" ID="Freemind_Link_1005249086" MODIFIED="1301353549218" TEXT="You should complement their positive actiions"/>
+<node CREATED="1301353550375" ID="Freemind_Link_566372554" MODIFIED="1301353563718" TEXT="You should complement their choice by appearing in different channels"/>
+</node>
+<node CREATED="1301353761062" ID="Freemind_Link_1291182838" MODIFIED="1301353764718" TEXT="Autofollows">
+<node CREATED="1301353765218" ID="Freemind_Link_758796803" MODIFIED="1301353774000" TEXT="One takes the lead and others fullow automatically"/>
+</node>
+<node CREATED="1301353831703" ID="Freemind_Link_781362378" MODIFIED="1301353834578" TEXT="Like Effect">
+<node CREATED="1301353835281" ID="Freemind_Link_1070893660" MODIFIED="1301353850968" TEXT="Physical Attraction Leading to Halo Effect"/>
+<node CREATED="1301353851625" ID="Freemind_Link_1185635945" MODIFIED="1301353864828" TEXT="Like People Simillar To Us"/>
+<node CREATED="1301353872140" ID="Freemind_Link_1420759806" MODIFIED="1301353885687" TEXT="We Like People Who Complement Us"/>
+<node CREATED="1301353890328" ID="Freemind_Link_874687961" MODIFIED="1301353897078" TEXT="We Like People Familiar To Us"/>
+<node CREATED="1301353905078" ID="Freemind_Link_130530109" MODIFIED="1301353916937" TEXT="Increased Like&apos;s Induced By Joint Effort"/>
+</node>
+<node CREATED="1301353977671" FOLDED="true" ID="Freemind_Link_1618910099" MODIFIED="1301354199234" TEXT="Reciprocation and Complements">
+<node CREATED="1301353982765" ID="Freemind_Link_330792363" MODIFIED="1301353999218" TEXT="Comments and Quoting on Social Media"/>
+</node>
+<node CREATED="1301354078140" ID="Freemind_Link_669924209" MODIFIED="1301354080562" TEXT="Authority">
+<node CREATED="1301354120593" ID="Freemind_Link_131251643" MODIFIED="1301354121609" TEXT="Cialdini talks about &#x201c;The extreme willingness of adults to go to almost any lengths on the command of authority&#x2026;&#x201d;  "/>
+</node>
+<node CREATED="1301354235093" ID="Freemind_Link_821137508" MODIFIED="1301354238859" TEXT="Scarcity">
+<node CREATED="1301354239390" ID="Freemind_Link_957841071" MODIFIED="1301354243828" TEXT="Create Scarcity"/>
+</node>
+</node>
+</node>
+</map>
