aeab363c60f88d10c6f2a37ac25f38d23059ecf4
vergy <vergy>
2011-05-09 13:41:56 +0000

    Safety Commit

--- src/main/site/Marketing.mm
+++ src/main/site/Marketing.mm
@@ -3,48 +3,134 @@
 <node CREATED="1304339998591" ID="Freemind_Link_647673425" MODIFIED="1304341080207" TEXT="Marketing">
 <font NAME="Comic Sans MS" SIZE="12"/>
 <node CREATED="1304603299475" ID="Freemind_Link_1783261531" MODIFIED="1304603303771" POSITION="right" TEXT="2011">
-<node CREATED="1304603326396" ID="Freemind_Link_1527518165" MODIFIED="1304603330724" TEXT="Executive Summary">
-<node CREATED="1304603337849" ID="Freemind_Link_992763483" MODIFIED="1304603422238" TEXT="We started this with location based advertising.">
-<node CREATED="1304603413863" ID="Freemind_Link_641449312" MODIFIED="1304603415050" TEXT="That is where the money will always come in.">
-<node CREATED="1304603432425" ID="Freemind_Link_1618242978" MODIFIED="1304603724779" TEXT="=&gt; We will continue to build stuff with this locations as the base."/>
+<node CREATED="1304603326396" ID="Freemind_Link_1527518165" MODIFIED="1304689582242" TEXT="Executive Summary of Adimpression 2011">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1304603337849" ID="Freemind_Link_992763483" MODIFIED="1304689575213" TEXT="We all started this with location based advertising.">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1304603413863" ID="Freemind_Link_641449312" MODIFIED="1304689596425" TEXT="That is where the money will always come in.">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1304603432425" ID="Freemind_Link_1618242978" MODIFIED="1304688953115" TEXT="=&gt; We will continue to build a location based advertising empire"/>
 </node>
-<node CREATED="1304603536329" ID="Freemind_Link_1918917941" MODIFIED="1304603655827" TEXT="This is where the core values of our products lie and always will.">
-<node CREATED="1304603561829" ID="Freemind_Link_111487237" MODIFIED="1304603600156" TEXT="This should be true to our valuable customers just as much it is to us.">
+<node CREATED="1304603536329" ID="Freemind_Link_1918917941" MODIFIED="1304689596425" TEXT="This is where the core values of our products lie in, and always will at..">
+<font NAME="SansSerif" SIZE="14"/>
 <node CREATED="1304603673545" ID="Freemind_Link_417404011" MODIFIED="1304603705201" TEXT="=&gt; We will continue to build stuff that values the customers location of interest."/>
 </node>
 </node>
-</node>
-<node CREATED="1304603804465" ID="Freemind_Link_1945463172" MODIFIED="1304603815418" TEXT="We currently specialize in two areas.">
-<node CREATED="1304603815886" ID="Freemind_Link_220727892" MODIFIED="1304603835245" TEXT="The first is making people socially rich">
-<node CREATED="1304603835855" ID="Freemind_Link_1279910710" MODIFIED="1304603858933" TEXT="There by brining in marketing possibilities.">
+<node CREATED="1304603804465" ID="Freemind_Link_1945463172" MODIFIED="1304689575228" TEXT="We currently specialize in two areas.">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304603815886" ID="Freemind_Link_220727892" MODIFIED="1304691016853" TEXT="The first is making people socially rich">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1304603835855" ID="Freemind_Link_1279910710" MODIFIED="1304689055042" TEXT="There by harvesting marketing approaches.">
 <node CREATED="1304603859761" ID="Freemind_Link_1626724262" MODIFIED="1304603885057" TEXT="This is a consistent give-take approach and will bring in profit."/>
 </node>
 </node>
-<node CREATED="1304603888573" ID="Freemind_Link_1331155267" MODIFIED="1304603901666" TEXT="The second is location based travel solutions">
-<node CREATED="1304603903135" ID="Freemind_Link_197747700" MODIFIED="1304603919603" TEXT="All travel websites are location specific.">
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304603888573" ID="Freemind_Link_1331155267" MODIFIED="1304691062151" TEXT="The second is location based travel solutions">
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304603903135" ID="Freemind_Link_197747700" MODIFIED="1304691062151" TEXT="All travel websites are location specific.">
 <node CREATED="1304603920134" ID="Freemind_Link_766723760" MODIFIED="1304603935915" TEXT="We need a good strategy to sell here.">
-<node CREATED="1304603936322" ID="Freemind_Link_1776698089" MODIFIED="1304603947118" TEXT="Possibly integrating with socializing."/>
+<node CREATED="1304603936322" ID="Freemind_Link_1776698089" MODIFIED="1304689082611" TEXT="This will be given only one single approach.">
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304689083175" ID="Freemind_Link_1826708889" MODIFIED="1304691062151" TEXT="Make things SIMPLE."/>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1304603982102" ID="Freemind_Link_1629765040" MODIFIED="1304603994742" TEXT="We need to bring in two things.">
-<node CREATED="1304603999852" ID="Freemind_Link_1207186241" MODIFIED="1304604009914" TEXT="Users">
-<node CREATED="1304604072678" ID="Freemind_Link_1471120910" MODIFIED="1304604252641" TEXT="Leverage free &#xa;online event publishing, invites and email forwarding invites &#xa;for Event Holders though the event pages."/>
-<node CREATED="1304604090491" ID="Freemind_Link_628389073" MODIFIED="1304604301845" TEXT="By some means, provide a richer socializing media for users">
+</node>
+<node CREATED="1304603982102" ID="Freemind_Link_1629765040" MODIFIED="1304689575213" TEXT="We need work on the one and only critical problem this year">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304603999852" ID="Freemind_Link_1207186241" MODIFIED="1304691062151" TEXT="Getting A LOT OF Users">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1304689234799" ID="Freemind_Link_1306505151" MODIFIED="1304689239553" TEXT="This includes">
+<node CREATED="1304604072678" ID="Freemind_Link_1471120910" MODIFIED="1304689245304" TEXT="leveraging free online event publishing, &#xa;invites and email forwardings  &#xa;for Event Holders though the event pages."/>
+<node CREATED="1304604090491" ID="Freemind_Link_628389073" MODIFIED="1304689652551" TEXT="By some means, providing a richer socializing media for users">
 <node CREATED="1304604303096" ID="Freemind_Link_1125308312" MODIFIED="1304604344219" TEXT="This could be better maps or fluid like navigation or anything good."/>
 </node>
-</node>
-<node CREATED="1304603997211" ID="Freemind_Link_1379397989" MODIFIED="1304603999149" TEXT="Profit">
-<node CREATED="1304604012070" ID="Freemind_Link_1145280393" MODIFIED="1304604043866" TEXT="To leverage profit we will use Google Advertising."/>
-<node CREATED="1304604044507" ID="Freemind_Link_247756907" MODIFIED="1304604071678" TEXT="To cater for future profit, we will implement several strategies."/>
+<node CREATED="1304604732460" ID="Freemind_Link_649662701" MODIFIED="1304689223390" TEXT="Using really rugged marketing to get the targetted user base and event vendors."/>
+<node CREATED="1304687634868" ID="Freemind_Link_1928081583" MODIFIED="1304689233225" TEXT="Creating an Android App"/>
 </node>
 </node>
 </node>
-<node CREATED="1304603305990" ID="Freemind_Link_620714991" MODIFIED="1304603316912" TEXT="05">
-<node CREATED="1304340068495" FOLDED="true" ID="Freemind_Link_84905687" MODIFIED="1304341080207" TEXT="20110502">
+<node CREATED="1304604599790" ID="Freemind_Link_30498905" MODIFIED="1304689620490" TEXT="We will be">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304604608855" ID="Freemind_Link_1086186781" MODIFIED="1304691062167" TEXT="At 1/4 million users by 2012 April">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#990000" COLOR="#ffffff" CREATED="1304604663186" ID="Freemind_Link_168439518" MODIFIED="1304691062167" TEXT="Supporting At Least 750 Event Vendors To Publish Events">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1304783075507" ID="Freemind_Link_140176817" MODIFIED="1304783088445" TEXT="Marketing Plan of Adimpression 2011">
+<node CREATED="1304783089007" ID="Freemind_Link_1359848668" MODIFIED="1304826980632" TEXT="The Almost Flat Marketing List">
+<node CREATED="1304783100008" ID="Freemind_Link_146684658" MODIFIED="1304783106774" TEXT="Article Submissions">
+<node CREATED="1304783107070" ID="Freemind_Link_531488876" MODIFIED="1304783138634" TEXT="Make sure you submit to only your website related sites"/>
+<node CREATED="1304783139915" ID="Freemind_Link_771209674" MODIFIED="1304783146337" TEXT="Signup for all travel forums"/>
+<node CREATED="1304783225887" ID="Freemind_Link_1766898548" MODIFIED="1304783231512" TEXT="Mix keywords"/>
+<node CREATED="1304783306764" ID="Freemind_Link_554993334" MODIFIED="1304783363125" TEXT="Keyword Rich Author Bio With Two Full HTTP Links to Your Site on anchor and one plain text"/>
+<node CREATED="1304783410002" ID="Freemind_Link_1318746775" MODIFIED="1304783415596" TEXT="500-800 words"/>
+<node CREATED="1304783431377" ID="Freemind_Link_11109893" MODIFIED="1304784404871" TEXT="Title:Top 10 ways of doing something does well OR How to "/>
+<node CREATED="1304783486473" ID="Freemind_Link_563746600" MODIFIED="1304783501504" TEXT="Ezine articles is value for time invested on this."/>
+<node CREATED="1304783518505" ID="Freemind_Link_1756800208" MODIFIED="1304783537630" TEXT="Make sure you do co-branding by including facebook.com or alike with ilikeplaces.com"/>
+<node CREATED="1304783749292" ID="Freemind_Link_1225937860" MODIFIED="1304783776214" TEXT="iSnare at 2$ for submission and article distribution"/>
+</node>
+</node>
+<node CREATED="1304826823756" ID="Freemind_Link_1622011936" MODIFIED="1304826974351" TEXT="The Half Done Marketing Errands List">
+<node CREATED="1304826982351" ID="Freemind_Link_484328439" MODIFIED="1304826999960" TEXT="Forums Interactions">
+<node CREATED="1304827000476" ID="Freemind_Link_1385656818" MODIFIED="1304827008367" TEXT="Available Forums">
+<node CREATED="1304827169883" ID="Freemind_Link_1854199894" LINK="http://www.party411.com" MODIFIED="1304827814740" TEXT="http://www.party411.com">
+<node CREATED="1304827192743" ID="Freemind_Link_1112416946" MODIFIED="1304827399415" TEXT="auth">
+<node CREATED="1304827401587" ID="Freemind_Link_1697321782" MODIFIED="1304827408102" TEXT="ravindranathakila"/>
+<node CREATED="1304827415587" ID="Freemind_Link_858118593" MODIFIED="1304827420133" TEXT="ravindranathakila@gmail.com"/>
+<node CREATED="1304827422290" ID="Freemind_Link_1811555687" MODIFIED="1304827426118" TEXT="teddy bear"/>
+<node CREATED="1304827846974" ID="Freemind_Link_1229045851" MODIFIED="1304827850302" TEXT="THIS IS FOR FORUM"/>
+</node>
+<node CREATED="1304827433836" ID="Freemind_Link_1754349921" MODIFIED="1304827441133" TEXT="bio">
+<node CREATED="1304827441821" ID="Freemind_Link_1536799954" MODIFIED="1304827444008" TEXT="Eventy"/>
+</node>
+<node CREATED="1304827446758" ID="Freemind_Link_415601618" MODIFIED="1304827453414" TEXT="settings">
+<node CREATED="1304827453789" ID="Freemind_Link_1986866278" MODIFIED="1304827456914" TEXT="Email Hidden"/>
+<node CREATED="1304828397302" ID="Freemind_Link_1997346368" MODIFIED="1304828404302" TEXT="Go to Messages to Edit Profile"/>
+</node>
+<node CREATED="1304827707850" ID="Freemind_Link_1129386015" MODIFIED="1304827710819" TEXT="interaction?">
+<node CREATED="1304827713116" ID="Freemind_Link_975099546" MODIFIED="1304827719975" TEXT="RSS on Forum Available"/>
+</node>
+</node>
+<node CREATED="1304830282996" ID="Freemind_Link_782487781" LINK="http://www.eventplanningforum.net/" MODIFIED="1304830290450" TEXT="http://www.eventplanningforum.net/">
+<node CREATED="1304830292529" ID="Freemind_Link_202768347" MODIFIED="1304830293623" TEXT="auth">
+<node CREATED="1304827415587" ID="Freemind_Link_1186467456" MODIFIED="1304827420133" TEXT="ravindranathakila@gmail.com"/>
+<node CREATED="1304827422290" ID="Freemind_Link_1946953760" MODIFIED="1304827426118" TEXT="teddy bear"/>
+<node CREATED="1304831285600" ID="Freemind_Link_1424986295" MODIFIED="1304831290913" TEXT="avaiting approval"/>
+</node>
+<node CREATED="1304830816958" ID="Freemind_Link_74663706" MODIFIED="1304830893219" TEXT="bio">
+<node CREATED="1304830819428" ID="Freemind_Link_26757948" MODIFIED="1304830821053" TEXT="Eventy"/>
+</node>
+</node>
+<node CREATED="1304830430297" ID="Freemind_Link_1980693272" LINK="http://forums.about.com/ab-eventplan" MODIFIED="1304830477225" TEXT="http://forums.about.com/ab-eventplan">
+<node CREATED="1304830869856" ID="Freemind_Link_1297641317" MODIFIED="1304830871560" TEXT="auth">
+<node CREATED="1304831195697" ID="Freemind_Link_1978938323" MODIFIED="1304831201198" TEXT="ravindranathakila@gmail.com"/>
+<node CREATED="1304830872028" ID="Freemind_Link_1036941490" MODIFIED="1304831148378" TEXT="teddybear"/>
+<node CREATED="1304830988372" ID="Freemind_Link_150596137" MODIFIED="1304831012407" TEXT="pet name doggy"/>
+</node>
+<node CREATED="1304830884671" ID="Freemind_Link_292461821" MODIFIED="1304830886905" TEXT="bio">
+<node CREATED="1304830887343" ID="Freemind_Link_514872893" MODIFIED="1304831194884" TEXT="EventHybrid"/>
+<node CREATED="1304830971698" ID="Freemind_Link_712012494" MODIFIED="1304830981856" TEXT="1996-04-02"/>
+</node>
+<node CREATED="1304831268582" ID="Freemind_Link_303836416" MODIFIED="1304831273192" TEXT="interaction?">
+<node CREATED="1304831273598" ID="Freemind_Link_1571547604" MODIFIED="1304831279208" TEXT="Sorty by high interest etc."/>
+<node CREATED="1304831472312" ID="Freemind_Link_360910743" MODIFIED="1304831476063" TEXT="http://forums.about.com/n/pfx/forum.aspx?tsn=4&amp;nav=messages&amp;webtag=ab-eventplan&amp;tid=207"/>
+</node>
+</node>
+<node CREATED="1304830587318" ID="Freemind_Link_430130717" MODIFIED="1304830592006" TEXT="www.eventeducation.com"/>
+<node CREATED="1304831452622" ID="Freemind_Link_150051005" MODIFIED="1304831455935" TEXT="http://www.funfilms.in/event-management/"/>
+<node CREATED="1304833125722" ID="Freemind_Link_665449830" MODIFIED="1304833126628" TEXT="http://eventplanning.about.com/od/eventplanningbasics/u/plan_an_event.htm"/>
+</node>
+<node CREATED="1304827559054" ID="Freemind_Link_863134497" MODIFIED="1304827644148" TEXT="Gmail Label: #eventy"/>
+</node>
+</node>
+</node>
+<node CREATED="1304603305990" FOLDED="true" ID="Freemind_Link_620714991" MODIFIED="1304604565950" TEXT="May">
+<node CREATED="1304340068495" ID="Freemind_Link_84905687" MODIFIED="1304341080207" TEXT="20110502">
 <font NAME="Comic Sans MS" SIZE="12"/>
-<node BACKGROUND_COLOR="#009900" CREATED="1304340062667" ID="_" MODIFIED="1304343571926" TEXT="Alien Analysis">
+<node BACKGROUND_COLOR="#009900" CREATED="1304340062667" FOLDED="true" ID="_" MODIFIED="1304343571926" TEXT="Alien Analysis">
 <font NAME="Comic Sans MS" SIZE="12"/>
 <node CREATED="1304340097086" ID="Freemind_Link_1823445291" MODIFIED="1304341080192" TEXT="Home Page">
 <font NAME="Comic Sans MS" SIZE="12"/>
@@ -134,7 +220,7 @@
 </node>
 </node>
 </node>
-<node BACKGROUND_COLOR="#cc00cc" CREATED="1304342734117" ID="Freemind_Link_1831760766" MODIFIED="1304343546132" TEXT="Perception Assumption">
+<node BACKGROUND_COLOR="#cc00cc" CREATED="1304342734117" FOLDED="true" ID="Freemind_Link_1831760766" MODIFIED="1304343546132" TEXT="Perception Assumption">
 <node BACKGROUND_COLOR="#ffff66" CREATED="1304342996087" ID="Freemind_Link_1253337406" MODIFIED="1304343645181" TEXT="This website is a professional site like or better than facebook"/>
 <node BACKGROUND_COLOR="#ffff66" CREATED="1304343015545" ID="Freemind_Link_99923607" MODIFIED="1304343645212" TEXT="This website is very useful to socialize"/>
 <node BACKGROUND_COLOR="#ffff66" CREATED="1304343026619" ID="Freemind_Link_1529051317" MODIFIED="1304343645212" TEXT="This website is very easy to use"/>
@@ -146,13 +232,13 @@
 <node BACKGROUND_COLOR="#ffff66" CREATED="1304343114225" ID="Freemind_Link_44826261" MODIFIED="1304343645196" TEXT="This website is free"/>
 <node BACKGROUND_COLOR="#ffff66" CREATED="1304343439453" ID="Freemind_Link_191115272" MODIFIED="1304343645196" TEXT="This website is fast enough"/>
 </node>
-<node BACKGROUND_COLOR="#006666" CREATED="1304342765667" ID="Freemind_Link_1625219234" MODIFIED="1304343588697" TEXT="Perception Reality">
+<node BACKGROUND_COLOR="#006666" CREATED="1304342765667" FOLDED="true" ID="Freemind_Link_1625219234" MODIFIED="1304343588697" TEXT="Perception Reality">
 <node BACKGROUND_COLOR="#ff6666" CREATED="1304343276847" ID="Freemind_Link_680048775" MODIFIED="1304343624030" TEXT="People don&apos;t know what i like places is just by domain name"/>
 <node BACKGROUND_COLOR="#ff6666" CREATED="1304343300950" ID="Freemind_Link_218386664" MODIFIED="1304343624030" TEXT="People get confused how their liking about places relate to the site"/>
 <node BACKGROUND_COLOR="#ff6666" CREATED="1304343341351" ID="Freemind_Link_1176585683" MODIFIED="1304343624030" TEXT="People don&apos;t like reading what this website is for. It is too much effort. It does not appeal"/>
 <node BACKGROUND_COLOR="#ff6666" CREATED="1304343452075" ID="Freemind_Link_76413996" MODIFIED="1304343624030" TEXT="The website takes a considerable time to load"/>
 </node>
-<node BACKGROUND_COLOR="#ff6600" CREATED="1304343470798" ID="Freemind_Link_1926506341" MODIFIED="1304343675121" TEXT="Fix Plan">
+<node BACKGROUND_COLOR="#ff6600" CREATED="1304343470798" FOLDED="true" ID="Freemind_Link_1926506341" MODIFIED="1304343675121" TEXT="Fix Plan">
 <node CREATED="1304343736702" ID="Freemind_Link_375485771" MODIFIED="1304343760081" TEXT="1. Reduce number of pop up words from 60 to 20"/>
 <node CREATED="1304343763338" ID="Freemind_Link_419210214" MODIFIED="1304343803893" TEXT="2. Remove pink. Use blue for safety(as researcha also implies)"/>
 <node CREATED="1304343824747" ID="Freemind_Link_1209697365" MODIFIED="1304343831293" TEXT="3. Beutify Search Button"/>
@@ -162,7 +248,7 @@
 <node CREATED="1304343973453" ID="Freemind_Link_1629938598" MODIFIED="1304344011607" TEXT="7. Deliver the simple message &quot;Plan fun event with your friends at fun places&quot;."/>
 <node CREATED="1304346858820" ID="Freemind_Link_540909837" MODIFIED="1304346875135" TEXT="8. Remove distractive map tooltip"/>
 </node>
-<node BACKGROUND_COLOR="#00ff00" CREATED="1304343476710" ID="Freemind_Link_1012321619" MODIFIED="1304346765650" TEXT="Fix" VSHIFT="-12">
+<node BACKGROUND_COLOR="#00ff00" CREATED="1304343476710" FOLDED="true" ID="Freemind_Link_1012321619" MODIFIED="1304346765650" TEXT="Fix" VSHIFT="-12">
 <node CREATED="1304343973453" ID="Freemind_Link_1724077065" MODIFIED="1304344011607" TEXT="7. Deliver the simple message &quot;Plan fun event with your friends at fun places&quot;."/>
 <node CREATED="1304343736702" ID="Freemind_Link_125802262" MODIFIED="1304343760081" TEXT="1. Reduce number of pop up words from 60 to 20"/>
 <node CREATED="1304343763338" ID="Freemind_Link_1686370108" MODIFIED="1304343803893" TEXT="2. Remove pink. Use blue for safety(as researcha also implies)"/>
