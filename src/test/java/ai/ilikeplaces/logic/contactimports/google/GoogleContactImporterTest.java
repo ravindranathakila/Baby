96d15c0451a4809c71f3b19353e817ea87bda2ef
vergy <vergy>
2011-08-18 15:02:38 +0000

    [feature #17181289] Add Google contact import support

---
+++ src/test/java/ai/ilikeplaces/logic/contactimports/google/GoogleContactImporterTest.java
@@ -0,0 +1,19 @@
+package ai.ilikeplaces.logic.contactimports.google;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 8/17/11
+ * Time: 10:39 PM
+ */
+public class GoogleContactImporterTest {
+
+    @Test
+    public void test() throws Exception {
+
+        GoogleContactImporter.main(new String[]{"not used","not used"});
+
+    }
+}
