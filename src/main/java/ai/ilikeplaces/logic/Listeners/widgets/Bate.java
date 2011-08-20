78a5e5b2f48d6177ec12d0ee31b1151a2b2cdf00
vergy <vergy>
2011-08-20 19:20:37 +0000

    [feature #17258983] Add offline invite support to get a reverse invite in

--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/Bate.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/Bate.java
@@ -1,8 +1,12 @@
 package ai.ilikeplaces.logic.Listeners.widgets;
 
 import ai.ilikeplaces.doc.License;
+import ai.ilikeplaces.entities.HumansIdentity;
+import ai.ilikeplaces.entities.HumansNetPeople;
 import ai.ilikeplaces.exception.DBDishonourCheckedException;
 import ai.ilikeplaces.logic.Listeners.JSCodeToSend;
+import ai.ilikeplaces.logic.contactimports.ImportedContact;
+import ai.ilikeplaces.logic.contactimports.google.GoogleContactImporter;
 import ai.ilikeplaces.logic.crud.DB;
 import ai.ilikeplaces.logic.mail.SendMail;
 import ai.ilikeplaces.logic.validators.unit.Email;
@@ -14,9 +18,12 @@ import ai.ilikeplaces.servlets.Controller.Page;
 import ai.ilikeplaces.servlets.ServletLogin;
 import ai.ilikeplaces.util.*;
 import net.sf.oval.Validator;
+import net.sf.oval.exception.ConstraintsViolatedException;
 import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.event.NodePropertyTransport;
 import org.itsnat.core.html.ItsNatHTMLDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
@@ -24,6 +31,7 @@ import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.html.HTMLDocument;
 
 import java.text.MessageFormat;
+import java.util.*;
 
 /**
  * @author Ravindranath Akila
@@ -33,6 +41,18 @@ import java.text.MessageFormat;
 abstract public class Bate extends AbstractWidgetListener {
     private Email email;
     private Password password;
+
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String HASH = "#";
+    private static final String DEFAULT = "default";
+    final private Logger logger = LoggerFactory.getLogger(FindFriend.class.getName());
+
+    HumanId humanId;
+
+    RefObj<List<HumansIdentity>> matches = null;
+
+    Set<Email> emails;
+
 
     /**
      * @param request__
@@ -50,6 +70,82 @@ abstract public class Bate extends AbstractWidgetListener {
     protected void init(final Object... initArgs) {
         email = new Email("");
         password = new Password("");
+
+        matches = new Obj<List<HumansIdentity>>(new ArrayList<HumansIdentity>());
+        emails = new HashSet<Email>();
+
+        if (((HumanId) initArgs[0]).validate() == 0) {
+            this.humanId = ((HumanId) initArgs[0]);
+        } else {
+            throw new ConstraintsViolatedException(((HumanId) initArgs[0]).getViolations());
+        }
+
+        final HumansNetPeople herExistingFriends = DB.getHumanCRUDHumanLocal(true).doDirtyRHumansNetPeople(humanId);//First important variable to notice
+
+        UCAttemptToRecognizeGoogleContactImportRedirect:
+        {
+
+            final String authToken = request.getServletRequest().getParameter(ACCESS_TOKEN);
+
+            if (authToken != null) {
+
+                final List<ImportedContact> whoppingImportedContacts;//Second important variable to notice
+                Import_Google_Contacts__Add_To_Emails_List:
+                {
+                    whoppingImportedContacts = GoogleContactImporter.fetchContacts(DEFAULT, authToken);
+
+                    for (final ImportedContact importedContact : whoppingImportedContacts) {//LOOPING A WHOPPING 1000
+                        emails.add(new Email(importedContact.getHumanId()));
+                    }
+                }
+
+                final List<HumansIdentity> existingUsersFromDB;//Third important variable to notice
+                final List<String> existingUsers = new ArrayList<String>();//LOOPING 300 Approx
+                Get_Users_Existing_Friends__Add_To_Humans_Network:
+                {
+                    existingUsersFromDB = DB.getHumanCRUDHumanLocal(true).doDirtyRHumansIdentitiesByEmails(new ArrayList<Email>(emails));
+                    for (final HumansNetPeople h : herExistingFriends.getHumansNetPeoples()) {
+                        existingUsers.add(h.getHumanId());
+                    }
+                }
+
+                clear($$(Controller.Page.BateImportResults));
+
+                for (final HumansIdentity humansIdentity : existingUsersFromDB) {
+                    if (!existingUsers.contains(humansIdentity.getHumanId())) {
+                        generateFriendAddWidgetFor(new HumanId(humansIdentity.getHumanId()), humanId);
+                    } else {
+                        generateFriendDeleteWidgetFor(new HumanId(humansIdentity.getHumanId()), humanId);
+                    }
+                }
+
+                for (final ImportedContact importedContact : whoppingImportedContacts) {
+                    if (!herExistingFriends.getHumansNetPeoples().contains(importedContact)) {
+                        new UserProperty(request,
+                                $$(Controller.Page.BateImportResults),
+                                importedContact.getFullName(),
+                                HASH,
+                                HASH,
+                                ElementComposer.compose($$(MarkupTag.DIV)).$ElementSetText("").get()) {
+                        };
+                    }
+                }
+            }
+        }
+
+        UCDisplayYouHaventAddedFriendsYet:
+        {
+            if (herExistingFriends.getHumansNetPeoples().size() == 0) {
+                $$(Controller.Page.friendFindSearchNotice).setTextContent("Oops! You haven't added any friends yet. Add your friends to get going!");
+            }
+        }
+
+        List<String> emails = new ArrayList<String>();
+        for (final HumansNetPeople hnp : herExistingFriends.getHumansNetPeoples()) {
+            emails.add(hnp.getHumanId());
+        }
+
+
     }
 
     @Override
@@ -139,5 +235,22 @@ abstract public class Bate extends AbstractWidgetListener {
         }
     }
 
+    private void generateFriendDeleteWidgetFor(final HumanId humanIdWhosProfileToShow, final HumanId currentUser) {
+        new UserProperty(request, $$(Page.friendFindSearchResults), humanIdWhosProfileToShow, currentUser) {
+            protected void init(final Object... initArgs) {
+                new FriendDelete(request, $$(Page.user_property_content), (HumanId) initArgs[0], (HumanId) ((Object[]) initArgs[1])[0]) {//This var args thingy is an awesome way to flexibility, except when not careful!
+                };
+            }
+        };
+    }
+
+    private void generateFriendAddWidgetFor(final HumanId humanIdWhosProfileToShow, final HumanId currentUser) {
+        new UserProperty(request, $$(Page.friendFindSearchResults), humanIdWhosProfileToShow, currentUser) {
+            protected void init(final Object... initArgs) {
+                new FriendAdd(request, $$(Page.user_property_content), (HumanId) initArgs[0], (HumanId) ((Object[]) initArgs[1])[0]) {//This var args thingy is an awesome way to flexibility, except when not careful!
+                };
+            }
+        };
+    }
 
 }
