f36c5b7f1450b73ba3e42227a6f328f4fd7101a2
vergy <vergy>
2011-09-20 15:43:22 +0000

    Formatted and commented

--- src/main/java/ai/ilikeplaces/entities/PrivateEvent.java
+++ src/main/java/ai/ilikeplaces/entities/PrivateEvent.java
@@ -19,6 +19,15 @@ import java.util.List;
             note = "We need to return this entity to the user for CRUD, hence cascade creation not possible.")
 @Entity
 public class PrivateEvent implements RefreshData<PrivateEvent> {
+// ------------------------------ FIELDS ------------------------------
+
+    final static public String privateEventOwnersCOL = "privateEventOwners";
+    final static public String privateEventViewersCOL = "privateEventViewers";
+    final static public String privateEventInvitesCOL = "privateEventInvites";
+    final static public String privateEventRejectsCOL = "privateEventRejects";
+    final static public String privateLocationCOL = "privateLocation";
+    final static public String locationCOL = "location";
+    final static public String privateEventAlbumCOL = "privateEventAlbum";
 
     public Long privateEventId;
 
@@ -35,127 +44,20 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
     public Wall privateEventWall;
 
     public List<HumansPrivateEvent> privateEventOwners;
-    final static public String privateEventOwnersCOL = "privateEventOwners";
 
     public List<HumansPrivateEvent> privateEventViewers;
-    final static public String privateEventViewersCOL = "privateEventViewers";
 
     public List<HumansPrivateEvent> privateEventInvites;
-    final static public String privateEventInvitesCOL = "privateEventInvites";
 
     public List<HumansPrivateEvent> privateEventRejects;
-    final static public String privateEventRejectsCOL = "privateEventRejects";
 
     public PrivateLocation privateLocation;
-    final static public String privateLocationCOL = "privateLocation";
 
     public Location location;
-    final static public String locationCOL = "location";
 
     public Album privateEventAlbum;
-    final static public String privateEventAlbumCOL = "privateEventAlbum";
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getPrivateEventId() {
-        return privateEventId;
-    }
-
-    public void setPrivateEventId(final Long privateEventId) {
-        this.privateEventId = privateEventId;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateEventIdR(final Long privateEventId) {
-        setPrivateEventId(privateEventId);
-        return this;
-    }
-
-    public String getPrivateEventName() {
-        return privateEventName;
-    }
-
-    public void setPrivateEventName(String privateEventName) {
-        this.privateEventName = privateEventName;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateEventNameR(String privateEventName) {
-        setPrivateEventName(privateEventName);
-        return this;
-    }
-
-    public String getPrivateEventInfo() {
-        return privateEventInfo;
-    }
-
-    public void setPrivateEventInfo(String privateEventInfo) {
-        this.privateEventInfo = privateEventInfo;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateEventInfoR(String privateEventInfo) {
-        setPrivateEventInfo(privateEventInfo);
-        return this;
-    }
-
-    public String getPrivateEventStartDate() {
-        return privateEventStartDate;
-    }
-
-    public void setPrivateEventStartDate(String privateEventStartDate) {
-        this.privateEventStartDate = privateEventStartDate;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateEventStartDateR(String privateEventStartDate) {
-        setPrivateEventStartDate(privateEventStartDate);
-        return this;
-    }
-
-    public String getPrivateEventEndDate() {
-        return privateEventEndDate;
-    }
-
-    public void setPrivateEventEndDate(String privateEventEndDate) {
-        this.privateEventEndDate = privateEventEndDate;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateEventEndDateR(String privateEventEndDate) {
-        setPrivateEventEndDate(privateEventEndDate);
-        return this;
-    }
-
-    public Boolean isExtendedAccess() {
-        return extendedAccess;
-    }
-
-    public void setExtendedAccess(Boolean extendedAccess) {
-        this.extendedAccess = extendedAccess;
-    }
-
-    @Transient
-    public PrivateEvent setExtendedAccessR(Boolean extendedAccess) {
-        setExtendedAccess(extendedAccess);
-        return this;
-    }
-
-    @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
-    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
-    public PrivateLocation getPrivateLocation() {
-        return privateLocation;
-    }
-
-    public void setPrivateLocation(PrivateLocation privateLocation) {
-        this.privateLocation = privateLocation;
-    }
-
-    @Transient
-    public PrivateEvent setPrivateLocationR(PrivateLocation privateLocation) {
-        this.privateLocation = privateLocation;
-        return this;
-    }
+// ------------------------ ACCESSORS / MUTATORS ------------------------
 
     @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
@@ -165,12 +67,6 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
 
     public void setLocation(Location location) {
         this.location = location;
-    }
-
-    @Transient
-    public PrivateEvent setLocationR(Location location) {
-        setLocation(location);
-        return this;
     }
 
     @UNIDIRECTIONAL
@@ -183,10 +79,30 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
         this.privateEventAlbum = privateEventAlbum;
     }
 
-    @Transient
-    public PrivateEvent setPrivateEventAlbumR(Album privateEventAlbum) {
-        this.privateEventAlbum = privateEventAlbum;
-        return this;
+    public String getPrivateEventEndDate() {
+        return privateEventEndDate;
+    }
+
+    public void setPrivateEventEndDate(String privateEventEndDate) {
+        this.privateEventEndDate = privateEventEndDate;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getPrivateEventId() {
+        return privateEventId;
+    }
+
+    public void setPrivateEventId(final Long privateEventId) {
+        this.privateEventId = privateEventId;
+    }
+
+    public String getPrivateEventInfo() {
+        return privateEventInfo;
+    }
+
+    public void setPrivateEventInfo(String privateEventInfo) {
+        this.privateEventInfo = privateEventInfo;
     }
 
     @WARNING(warning = "Owner because once an event needs to be deleted, deleting this entity is easier if owner." +
@@ -201,18 +117,16 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
         this.privateEventInvites = privateEventInvites;
     }
 
-    @WARNING(warning = "Owner because once an event needs to be deleted, deleting this entity is easier if owner." +
-            "If this entity is not the owner, individual owner viewer accepteee rejectee will have to delete their events individually.")
-    @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
-    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
-    public List<HumansPrivateEvent> getPrivateEventViewers() {
-        return privateEventViewers;
+    public String getPrivateEventName() {
+        return privateEventName;
     }
 
-    public void setPrivateEventViewers(final List<HumansPrivateEvent> privateEventViewers) {
-        this.privateEventViewers = privateEventViewers;
+    public void setPrivateEventName(String privateEventName) {
+        this.privateEventName = privateEventName;
     }
 
+    @NOTE("Viewers of a private event have no implication except that they could be used as 'people who can see shared items' in future. " +
+            "So far, there is no use of this. We are hiding it for this release 2011/09/19 in the user interface")
     @WARNING(warning = "Owner because once an event needs to be deleted, deleting this entity is easier if owner." +
             "If this entity is not the owner, individual owner viewer accepteee rejectee will have to delete their events individually.")
     @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
@@ -237,6 +151,26 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
         this.privateEventRejects = privateEventRejects;
     }
 
+    public String getPrivateEventStartDate() {
+        return privateEventStartDate;
+    }
+
+    public void setPrivateEventStartDate(String privateEventStartDate) {
+        this.privateEventStartDate = privateEventStartDate;
+    }
+
+    @WARNING(warning = "Owner because once an event needs to be deleted, deleting this entity is easier if owner." +
+            "If this entity is not the owner, individual owner viewer accepteee rejectee will have to delete their events individually.")
+    @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    public List<HumansPrivateEvent> getPrivateEventViewers() {
+        return privateEventViewers;
+    }
+
+    public void setPrivateEventViewers(final List<HumansPrivateEvent> privateEventViewers) {
+        this.privateEventViewers = privateEventViewers;
+    }
+
     @UNIDIRECTIONAL
     @OneToOne(cascade = CascadeType.ALL)
     public Wall getPrivateEventWall() {
@@ -247,19 +181,28 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
         this.privateEventWall = privateEventWall;
     }
 
-    @Transient
-    public PrivateEvent setPrivateEventWallR(final Wall privateEventWall) {
-        this.privateEventWall = privateEventWall;
-        return this;
+    @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    public PrivateLocation getPrivateLocation() {
+        return privateLocation;
     }
 
-    @Override
-    public String toString() {
-        return "PrivateEvent{" +
-                ", privateEventId=" + privateEventId +
-                ", privateLocation=" + privateLocation +
-                '}';
+    public void setPrivateLocation(PrivateLocation privateLocation) {
+        this.privateLocation = privateLocation;
     }
+
+    public Boolean isExtendedAccess() {
+        return extendedAccess;
+    }
+
+    public void setExtendedAccess(Boolean extendedAccess) {
+        this.extendedAccess = extendedAccess;
+    }
+
+// ------------------------ INTERFACE METHODS ------------------------
+
+
+// --------------------- Interface RefreshData ---------------------
 
     /**
      * Calling this method will refresh any lazily fetched lists in this entity making them availabe for use.
@@ -280,4 +223,75 @@ public class PrivateEvent implements RefreshData<PrivateEvent> {
         return this;
     }
 
+// ------------------------ CANONICAL METHODS ------------------------
+
+    @Override
+    public String toString() {
+        return "PrivateEvent{" +
+                ", privateEventId=" + privateEventId +
+                ", privateLocation=" + privateLocation +
+                '}';
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Transient
+    public PrivateEvent setExtendedAccessR(Boolean extendedAccess) {
+        setExtendedAccess(extendedAccess);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setLocationR(Location location) {
+        setLocation(location);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventAlbumR(Album privateEventAlbum) {
+        this.privateEventAlbum = privateEventAlbum;
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventEndDateR(String privateEventEndDate) {
+        setPrivateEventEndDate(privateEventEndDate);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventIdR(final Long privateEventId) {
+        setPrivateEventId(privateEventId);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventInfoR(String privateEventInfo) {
+        setPrivateEventInfo(privateEventInfo);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventNameR(String privateEventName) {
+        setPrivateEventName(privateEventName);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventStartDateR(String privateEventStartDate) {
+        setPrivateEventStartDate(privateEventStartDate);
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateEventWallR(final Wall privateEventWall) {
+        this.privateEventWall = privateEventWall;
+        return this;
+    }
+
+    @Transient
+    public PrivateEvent setPrivateLocationR(PrivateLocation privateLocation) {
+        this.privateLocation = privateLocation;
+        return this;
+    }
 }
