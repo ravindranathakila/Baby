b2ceb9c5c1233e49230142d960f6daee9b207b3f
vergy <vergy>
2011-10-20 16:15:44 +0000

    [feature #19951525] BTries - Creating Entities and Widgets

---
+++ src/main/java/ai/ilikeplaces/entities/HumansTribe.java
@@ -0,0 +1,48 @@
+package ai.ilikeplaces.entities;
+
+import ai.ilikeplaces.doc.BIDIRECTIONAL;
+import ai.ilikeplaces.doc.UNIDIRECTIONAL;
+import ai.ilikeplaces.doc.WARNING;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 10/12/11
+ * Time: 7:47 PM
+ */
+@WARNING("THIS ENTITY IS NOT GUARANTEED TO 'BE' EVEN THOUGH A HUMAN IS SIGNED UP. SO CREATE IT IF NOT PRESENT!")
+@Entity
+public class HumansTribe {
+    public String humanId;
+
+    public Set<Tribe> tribes;
+
+    @Id
+    public String getHumanId() {
+        return humanId;
+    }
+
+    public void setHumanId(final String humanId) {
+        this.humanId = humanId;
+    }
+
+    @Transient
+    public HumansTribe setHumanIdR(final String humanId) {
+        this.humanId = humanId;
+        return this;
+    }
+
+    @BIDIRECTIONAL(ownerside = BIDIRECTIONAL.OWNING.IS)
+    @WARNING(warning = "Owning as deleting a location should automatically reflect in humans, not vice versa.")
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    public Set<Tribe> getTribes() {
+        return tribes;
+    }
+
+    public void setTribes(final Set<Tribe> tribes) {
+        this.tribes = tribes;
+    }
+}
---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/TribeWidgetCriteria.java
@@ -0,0 +1,10 @@
+package ai.ilikeplaces.logic.Listeners.widgets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 10/20/11
+ * Time: 8:51 PM
+ */
+public class TribeWidgetCriteria {
+}
