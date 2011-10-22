d67f611b92dc192c53f8fbce77b11671b08dbb7b
vergy <vergy>
2011-10-22 13:37:52 +0000

    [feature #20029419] Provide Basic Help and FAQ Support

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/HelpCriteria.java
@@ -0,0 +1,10 @@
+package ai.ilikeplaces.logic.Listeners.widgets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 10/22/11
+ * Time: 11:04 AM
+ */
+public class HelpCriteria {
+}
