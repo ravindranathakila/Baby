a4548651c5576ad6850efa86aa88480c88f2ad82
vergy <vergy>
2011-10-26 15:59:59 +0000

    [feature #19951525] BTries - Creating Entities and Widgets DONE

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/ListenerTribes.java
@@ -0,0 +1,71 @@
+package ai.ilikeplaces.logic.Listeners;
+
+import ai.ilikeplaces.logic.Listeners.widgets.DownTownFlowCriteria;
+import ai.ilikeplaces.logic.Listeners.widgets.TribeWidget;
+import ai.ilikeplaces.logic.Listeners.widgets.TribeWidgetCriteria;
+import ai.ilikeplaces.servlets.Controller;
+import org.itsnat.core.ItsNatDocument;
+import org.itsnat.core.ItsNatServletRequest;
+import org.itsnat.core.ItsNatServletResponse;
+import org.itsnat.core.event.ItsNatServletRequestListener;
+import org.itsnat.core.html.ItsNatHTMLDocument;
+import org.w3c.dom.html.HTMLDocument;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 10/26/11
+ * Time: 8:40 PM
+ */
+public class ListenerTribes implements ItsNatServletRequestListener {
+    @Override
+    public void processRequest(final ItsNatServletRequest request__, final ItsNatServletResponse itsNatServletResponse) {
+        new AbstractSkeletonListener(request__) {
+            @Override
+            protected void init(ItsNatHTMLDocument itsNatHTMLDocument__, HTMLDocument hTMLDocument__, ItsNatDocument itsNatDocument__, Object... initArgs) {
+
+                layoutNeededForAllPages:
+                {
+                    setLoginWidget:
+                    {
+                        setLoginWidget(request__);
+                    }
+
+                    setTitle:
+                    {
+                        //set below
+                    }
+
+                    setProfileLink:
+                    {
+                        setProfileLink();
+                    }
+                    setProfileDataLink:
+                    {
+                        setProfileDataLink();
+                    }
+                    sideBarFriends:
+                    {
+                        setSideBarFriends(request__, DownTownFlowCriteria.DownTownFlowDisplayComponent.TRIBES);
+                    }
+                    signinupActionNotice:
+                    {
+                        //We do nothing now
+                    }
+                    SetNotifications:
+                    {
+                        setNotifications();
+                    }
+                }
+
+                if (getUsername() != null) {
+
+                    new TribeWidget(request__, new TribeWidgetCriteria(), $(Controller.Page.Skeleton_center_content));
+
+                } else {
+                    //WE COULD STILL GIVE A TRIBE CREATION WIDGET WITH USERS EMAIL AND FRIENDS EMAILS. ONCE USER SIGNS UP, THE TRIBE CAN BE CREATED.
+                }
+            }
+        };
+    }
+}
