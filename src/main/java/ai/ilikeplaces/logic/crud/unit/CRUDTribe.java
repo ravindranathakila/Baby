792caa3280a63831fdf201d9556cb0588f795d8a
vergy <vergy>
2011-10-27 14:05:42 +0000

    [feature #20240277] BTries - Creating CRUD classes

---
+++ src/main/java/ai/ilikeplaces/logic/crud/unit/CRUDTribe.java
@@ -0,0 +1,16 @@
+package ai.ilikeplaces.logic.crud.unit;
+
+import ai.ilikeplaces.doc.License;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 10/27/11
+ * Time: 7:33 PM
+ */
+@License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
+@Stateless
+public class CRUDTribe implements CRUDTribeLocal {
+}
