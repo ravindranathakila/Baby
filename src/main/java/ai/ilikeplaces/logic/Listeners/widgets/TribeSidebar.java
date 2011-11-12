2b80e67a8f9df3a5d86b3e1fb200c21fd53105d4
vergy <vergy>
2011-11-12 05:34:33 +0000

    [feature #20925869] Down Town Flow Tribes Widget DAMN, FORGOT TO ADD NEW FILES. BTW NOW TRIBE IMAGE IS HIDDEN TILL IT IS RESOLVED

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/TribeSidebar.java
@@ -0,0 +1,161 @@
+package ai.ilikeplaces.logic.Listeners.widgets;
+
+import ai.ilikeplaces.doc.License;
+import ai.ilikeplaces.doc.OK;
+import ai.ilikeplaces.entities.Msg;
+import ai.ilikeplaces.entities.Tribe;
+import ai.ilikeplaces.logic.Listeners.JSCodeToSend;
+import ai.ilikeplaces.logic.crud.DB;
+import ai.ilikeplaces.logic.validators.unit.HumanId;
+import ai.ilikeplaces.logic.validators.unit.VLong;
+import ai.ilikeplaces.rbs.RBGet;
+import ai.ilikeplaces.servlets.Controller.Page;
+import ai.ilikeplaces.util.*;
+import ai.ilikeplaces.util.cache.SmartCache2;
+import org.itsnat.core.ItsNatServletRequest;
+import org.itsnat.core.html.ItsNatHTMLDocument;
+import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.html.HTMLDocument;
+
+import static ai.ilikeplaces.servlets.Controller.Page.*;
+
+
+/**
+ * @author Ravindranath Akila
+ */
+
+@License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
+@OK
+public class TribeSidebar extends AbstractWidgetListener<TribeSidebarCriteria> {
+// ------------------------------ FIELDS ------------------------------
+
+
+// ------------------------------ FIELDS STATIC --------------------------
+
+    public static final SmartCache2<Long, Tribe, HumanId> TRIBE_BASIC_INFO_CACHE = new SmartCache2<Long, Tribe, HumanId>(
+            new SmartCache2.RecoverWith<Long, Tribe, HumanId>() {
+                @Override
+                public Tribe getValue(final Long tribeId, HumanId humanId) {
+
+                    return DB.getHumanCRUDTribeLocal(false).getTribe(humanId, new VLong(tribeId));
+                }
+            }
+    );
+
+    private static final String READ_MORE = "read.more";
+
+    // --------------------------- CONSTRUCTORS ---------------------------
+    public TribeSidebar(final ItsNatServletRequest request__, final TribeSidebarCriteria tribeSidebarCriteria, final Element appendToElement__) {
+
+        super(request__, Page.TribeSidebar, tribeSidebarCriteria, appendToElement__);
+    }
+
+// ------------------------ OVERRIDING METHODS ------------------------
+
+    /**
+     * @param tribeSidebarCriteria
+     */
+    @Override
+    protected void init(final TribeSidebarCriteria tribeSidebarCriteria) {
+
+        final HumanId humanId = new HumanId(tribeSidebarCriteria.getHumanId());
+        final long tribeId = tribeSidebarCriteria.getTribeId();
+
+
+        final Tribe tribe = TRIBE_BASIC_INFO_CACHE.get(tribeId, humanId);
+
+        final Msg lastWallEntry = WallWidgetTribe.LAST_WALL_ENTRY.get(tribe.getTribeWall().getWallId(), humanId.getHumanId());
+
+        final String href = new Parameter(Tribes.getURL())
+                .append(DocTribesMode, DocTribesModeView, true)
+                .append(DocTribesWhich, tribe.getTribeId())
+                .get();
+
+        $$(Page.tribe_sidebar_name).setTextContent(tribe.getTribeName());
+
+        //$$(Page.tribe_sidebar_profile_photo).setAttribute(MarkupTag.IMG.title(),"Tribe Photo Url");
+
+        if (lastWallEntry != null) {
+            new UserPropertySidebar(
+                    request,
+                    $$(Page.tribe_sidebar_content),
+                    new HumanId(lastWallEntry.getMsgMetadata()),
+                    lastWallEntry,
+                    href) {
+                private Msg mylastWallEntry;
+                private String myhref;
+
+                protected void init(final Object... initArgs) {
+
+                    mylastWallEntry = (Msg) ((Object[]) initArgs[1])[0];
+                    myhref = (String) ((Object[]) initArgs[1])[1];
+
+                    $$displayBlock($$(Page.user_property_sidebar_talk));
+
+                    String msgContent = lastWallEntry.getMsgContent();
+
+                    TrimMessageContentForReadabilityOnSidebar:
+                    {
+                        final int length = msgContent.length();
+                        if (40 < length) {
+                            msgContent = msgContent.substring(0, 40) + RBGet.gui().getString(READ_MORE);
+                        }
+                    }
+
+                    Element commentHref = ElementComposer.compose($$(MarkupTag.A)).$ElementSetText(msgContent).$ElementSetHref(myhref).get();
+                    $$(Page.user_property_sidebar_content).appendChild(commentHref);
+                }
+
+                @Override
+                protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
+
+                    itsNatHTMLDocument_.addEventListener((EventTarget) $$(user_property_sidebar_talk), EventType.CLICK.toString(), new EventListener() {
+                        @Override
+                        public void handleEvent(final Event evt_) {
+
+                            $$sendJS(JSCodeToSend.redirectPageWithURL(myhref));
+                        }
+                    }, false);
+                }
+            };
+        } else {
+            new UserPropertySidebar(
+                    request,
+                    $$(Page.tribe_sidebar_content),
+                    humanId,
+                    "",
+                    href) {
+                private String myhref;
+
+                protected void init(final Object... initArgs) {
+
+                    myhref = (String) ((Object[]) initArgs[1])[1];
+
+                    $$displayBlock($$(Page.user_property_sidebar_talk));
+                    Element commentHref = ElementComposer.compose($$(MarkupTag.A)).$ElementSetText("").$ElementSetHref(myhref).get();
+                    $$(Page.user_property_sidebar_content).appendChild(commentHref);
+                }
+
+                @Override
+                protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
+
+                    itsNatHTMLDocument_.addEventListener((EventTarget) $$(user_property_sidebar_talk), EventType.CLICK.toString(), new EventListener() {
+                        @Override
+                        public void handleEvent(final Event evt_) {
+
+                            $$sendJS(JSCodeToSend.redirectPageWithURL(myhref));
+                        }
+                    }, false);
+                }
+            };
+        }
+    }
+
+    @Override
+    protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
+
+    }
+}
