f9e87265340b81245728e1b33071e29f180e6c65
vergy <vergy>
2011-11-12 03:50:29 +0000

    no message

---
+++ src/main/site/PO.mm
@@ -0,0 +1,40 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#999999" CREATED="1320940914915" ID="ID_728474189" MODIFIED="1320972824122" TEXT="PO">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1320940957075" ID="ID_1320662555" MODIFIED="1320941065476" POSITION="right" TEXT="Y do we need words for the site">
+<icon BUILTIN="help"/>
+<node CREATED="1320941036723" ID="ID_1022512398" MODIFIED="1320941078899" TEXT="To convey meaning">
+<icon BUILTIN="yes"/>
+<node CREATED="1320941042661" ID="ID_1298034003" MODIFIED="1320941065476" TEXT="Y do we need words to convey meaning?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1320941114934" ID="ID_1021855344" MODIFIED="1320941129951" TEXT="Y don&apos;t we use pictures only">
+<icon BUILTIN="idea"/>
+<node CREATED="1320941196317" ID="ID_1006262942" MODIFIED="1320941401896" TEXT="@ - Email">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1320941204380" ID="ID_1639973249" MODIFIED="1320941401896" TEXT="Key - Password">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1320941213881" ID="ID_1504852892" MODIFIED="1320941401896" TEXT="Cloud - Talk">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1320941219459" ID="ID_1287532513" MODIFIED="1320941401896" TEXT="Musical Notes and Hands - Moments">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1320941247149" ID="ID_188365522" MODIFIED="1320941401896" TEXT="Tribal Hat - Tribes">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1320940972936" ID="ID_1869369656" MODIFIED="1320941065476" POSITION="left" TEXT="Y do we need visitors and retention">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1320941480966" ID="ID_1965947944" MODIFIED="1320941497764" POSITION="right" TEXT="Y can&apos;t the site get users?">
+<icon BUILTIN="help"/>
+<node CREATED="1320941619977" ID="ID_260274084" MODIFIED="1320941632681" TEXT="Y don&apos;t we get users the site"/>
+</node>
+</node>
+</map>
