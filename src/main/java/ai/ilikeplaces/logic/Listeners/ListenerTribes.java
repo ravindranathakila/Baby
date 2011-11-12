77a3b21a4156a6bbeff8e6f949224e3e5c28dd15
vergy <vergy>
2011-11-12 04:28:53 +0000

    [feature #20925473] Add support to show selective tribes in _tribes page. DONE, BUT LIST OF TRIBES CANNOT BE OBTAINED FROM ANYWHERE ON THE GUI. EITHER SHOULD GO TO THE FLOW OR ANOTHER MODE

--- src/main/java/ai/ilikeplaces/logic/Listeners/ListenerTribes.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/ListenerTribes.java
@@ -11,7 +11,13 @@ import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.ItsNatServletResponse;
 import org.itsnat.core.event.ItsNatServletRequestListener;
 import org.itsnat.core.html.ItsNatHTMLDocument;
+import org.w3c.dom.Element;
 import org.w3c.dom.html.HTMLDocument;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static ai.ilikeplaces.util.Loggers.USER_EXCEPTION;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,8 +26,15 @@ import org.w3c.dom.html.HTMLDocument;
  * Time: 8:40 PM
  */
 public class ListenerTribes implements ItsNatServletRequestListener {
+    private static final String COMMA = ",";
+    // ------------------------ INTERFACE METHODS ------------------------
+
+
+// --------------------- Interface ItsNatServletRequestListener ---------------------
+
     @Override
     public void processRequest(final ItsNatServletRequest request__, final ItsNatServletResponse itsNatServletResponse) {
+
         new AbstractSkeletonListener(request__) {
             @Override
             protected void init(ItsNatHTMLDocument itsNatHTMLDocument__, HTMLDocument hTMLDocument__, ItsNatDocument itsNatDocument__, Object... initArgs) {
@@ -62,25 +75,87 @@ public class ListenerTribes implements ItsNatServletRequestListener {
 
                 if (getUsername() != null) {
 
-                    for (final Tribe tribe : DB.getHumanCRUDTribeLocal(false).getHumansTribes(new HumanId(getUsernameAsValid()))) {
-                        new TribeWidget(
-                                request__,
-                                new TribeWidgetCriteria()
-                                        .setHumanId(geHumanIdAsValid())
-                                        .setTribeId(new VLong().setObjAsValid(tribe.getTribeId())),
-                                $(Controller.Page.Skeleton_center_skeleton));
+
+                    /*Be fault tolerant with the user. Just parse category here. Later parse other values when needed*/
+                    int mode = -1;
+                    try {
+                        final String modeString = request__.getServletRequest().getParameter(Controller.Page.DocTribesMode);
+                        mode = modeString != null ? Integer.parseInt(modeString) : Controller.Page.DocTribesModeCreate;
+                    } catch (final NumberFormatException nfe_) {
+                        USER_EXCEPTION.error("", nfe_);
                     }
+                    switch (mode) {
+                        case Controller.Page.DocTribesModeCreate: {
+                            showTribeCreateWidget(request__, getUsernameAsValid(), $(Controller.Page.Skeleton_center_skeleton));
+                            break;
+                        }
 
-                    new TribeCreateWidget(
-                            request__,
-                            new TribeCreateWidgetCriteria(
-                                    new HumanId(getUsernameAsValid())),
-                            $(Controller.Page.Skeleton_center_skeleton));
 
+                        case Controller.Page.DocTribesModeView: {
+
+                            try {
+                                final String whichString = request__.getServletRequest().getParameter(Controller.Page.DocTribesWhich);
+                                if (whichString != null) {
+                                    final String[] whichStringArr = whichString.split(COMMA);
+                                    final Set<Long> which = new HashSet<Long>(whichStringArr.length);
+
+                                    for (int i = 0, whichStringArrLength = whichStringArr.length; i < whichStringArrLength; i++) {
+                                        try {
+                                            which.add(Long.parseLong(whichStringArr[i].trim()));
+                                        } catch (NumberFormatException e) {
+                                            //Ignore this pathetic situation and add 0 which will never match with a tribe
+                                            which.add(0L);
+                                        }
+                                    }
+
+                                    final Set<Tribe> humansTribes;
+
+                                    FirstWeGetThisPersonsTribes_ThisIsImportant:
+                                    {
+                                        humansTribes = DB.getHumanCRUDTribeLocal(false).getHumansTribes(new HumanId(getUsernameAsValid()));
+                                    }
+
+                                    for (final Tribe tribe : humansTribes) {
+                                        if (which.contains(tribe.getTribeId())) {
+                                            new TribeWidget(
+                                                    request__,
+                                                    new TribeWidgetCriteria()
+                                                            .setHumanId(geHumanIdAsValid())
+                                                            .setTribeId(new VLong().setObjAsValid(tribe.getTribeId())),
+                                                    $(Controller.Page.Skeleton_center_skeleton));
+                                        }
+                                    }
+
+                                } else {
+                                    //We show the tribe create widget
+                                    showTribeCreateWidget(request__, getUsernameAsValid(), $(Controller.Page.Skeleton_center_skeleton));
+                                }
+
+                            } catch (final NumberFormatException nfe_) {
+                                USER_EXCEPTION.error("", nfe_);
+                            }
+
+
+                            break;
+                        }
+
+                        default: {
+                            showTribeCreateWidget(request__, getUsernameAsValid(), $(Controller.Page.Skeleton_center_skeleton));
+                            break;
+                        }
+                    }
                 } else {
                     //WE COULD STILL GIVE A TRIBE CREATION WIDGET WITH USERS EMAIL AND FRIENDS EMAILS. ONCE USER SIGNS UP, THE TRIBE CAN BE CREATED.
                 }
             }
         };
+    }
+
+    private void showTribeCreateWidget(ItsNatServletRequest request__, final String humanId, final Element appendToElement) {
+        new TribeCreateWidget(
+                request__,
+                new TribeCreateWidgetCriteria(
+                        new HumanId(humanId)),
+                appendToElement);
     }
 }
