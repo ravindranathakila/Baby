23a21180780ba1b6b5296e9d676660d1e6865ee3
vergy <vergy>
2011-11-27 07:47:41 +0000

    [feature #21492497] Teach the user what to do - Tribes DONE BUT NEED TO FIX SIGN UP CALL FLOW UPON ACTIVATION

--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/teach/TeachTribe.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/teach/TeachTribe.java
@@ -1,10 +1,22 @@
 package ai.ilikeplaces.logic.Listeners.widgets.teach;
 
+import ai.ilikeplaces.exception.DBDishonourCheckedException;
+import ai.ilikeplaces.logic.Listeners.JSCodeToSend;
+import ai.ilikeplaces.logic.Listeners.widgets.Bate;
+import ai.ilikeplaces.logic.Listeners.widgets.UserProperty;
+import ai.ilikeplaces.logic.contactimports.ImportedContact;
+import ai.ilikeplaces.logic.crud.DB;
+import ai.ilikeplaces.logic.mail.SendMail;
+import ai.ilikeplaces.logic.validators.unit.Email;
+import ai.ilikeplaces.logic.validators.unit.HumanId;
+import ai.ilikeplaces.logic.validators.unit.Password;
 import ai.ilikeplaces.servlets.Controller;
-import ai.ilikeplaces.util.AbstractWidgetListener;
+import ai.ilikeplaces.servlets.ServletLogin;
+import ai.ilikeplaces.util.*;
 import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.html.ItsNatHTMLDocument;
 import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
 import org.w3c.dom.html.HTMLDocument;
 
 /**
@@ -14,7 +26,29 @@ import org.w3c.dom.html.HTMLDocument;
  * Time: 8:07 AM
  */
 public class TeachTribe extends AbstractWidgetListener<TeachTribeCriteria> {
+// ------------------------------ FIELDS ------------------------------
 
+
+// ------------------------------ FIELDS STATIC --------------------------
+
+
+    private static final String PASSWORD_DETAILS = "_passwordDetails";
+    private static final String PASSWORD_ADVICE = "_passwordAdvice";
+    private static final String URL = "_url";
+
+// -------------------------- ENUMERATIONS --------------------------
+
+    public static enum TeachTribeIds implements WidgetIds {
+        teach_tribe_widget,
+        teach_tribe_noti,
+        teach_tribe_signed_in,
+        teach_tribe_signed_out,
+        teach_tribe_signup_section,
+        teach_tribe_signup_input,
+        teach_tribe_signup_click,
+    }
+
+// --------------------------- CONSTRUCTORS ---------------------------
 
     /**
      * @param request__
@@ -22,7 +56,31 @@ public class TeachTribe extends AbstractWidgetListener<TeachTribeCriteria> {
      * @param appendToElement__
      */
     public TeachTribe(final ItsNatServletRequest request__, final TeachTribeCriteria teachTribeCriteria, final Element appendToElement__) {
+
         super(request__, Controller.Page.TeachTribe, teachTribeCriteria, appendToElement__);
+    }
+
+// ------------------------ OVERRIDING METHODS ------------------------
+
+    /**
+     * Use this only in conjunction with {@link #AbstractWidgetListener(org.itsnat.core.ItsNatServletRequest, ai.ilikeplaces.servlets.Controller.Page, Object, org.w3c.dom.Element)}
+     * GENERIC constructor.
+     *
+     * @param teachTribeCriteria
+     */
+    @Override
+    protected void init(final TeachTribeCriteria teachTribeCriteria) {
+
+        super.registerUserNotifier($$(TeachTribeIds.teach_tribe_noti));
+
+        ShowHideWidgetAreaBasedOnLoggedInState:
+        {
+            if (teachTribeCriteria.getHumanId() == null) {
+                $$displayBlock($$(TeachTribeIds.teach_tribe_signed_out));
+            } else {
+                $$displayBlock($$(TeachTribeIds.teach_tribe_signed_in));
+            }
+        }
     }
 
     /**
@@ -34,12 +92,87 @@ public class TeachTribe extends AbstractWidgetListener<TeachTribeCriteria> {
      * @param hTMLDocument_
      */
     @Override
-    protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
+    protected void registerEventListeners(final ItsNatHTMLDocument itsNatHTMLDocument_, final HTMLDocument hTMLDocument_) {
 
-    }
+        super.registerForInputText($$(TeachTribeIds.teach_tribe_signup_input),
+                new AIEventListener<TeachTribeCriteria>(criteria) {
+                    /**
+                     * Override this method and avoid {@link #handleEvent(org.w3c.dom.events.Event)} to make debug logging transparent
+                     *
+                     * @param evt fired from client
+                     */
+                    @Override
+                    protected void onFire(Event evt) {
+
+                        criteria.getSignupCriteria().setEmail(new Email(getTargetInputText(evt)));
+                    }
+                }
+        );
+
+        super.registerForClick($$(TeachTribeIds.teach_tribe_signup_click),
+                new AIEventListener<TeachTribeCriteria>(criteria) {
+                    /**
+                     * Override this method and avoid {@link #handleEvent(org.w3c.dom.events.Event)} to make debug logging transparent
+                     *
+                     * @param evt fired from client
+                     */
+                    @Override
+                    protected void onFire(Event evt) {
+
+                        final Email myemail = criteria.getSignupCriteria().getEmail();
+
+                        if (myemail.valid()) {
+                            if (!DB.getHumanCRUDHumanLocal(true).doDirtyCheckHuman(myemail.getObj()).returnValue()) {
+                                try {
+                                    final String randomPassword = Long.toHexString(Double.doubleToLongBits(Math.random()));
+
+                                    final Return<Boolean> humanCreateReturn = DB.getHumanCRUDHumanLocal(true).doCHuman(
+                                            new HumanId().setObjAsValid(myemail.getObj()),
+                                            new Password(randomPassword),
+                                            myemail);
+
+                                    if (humanCreateReturn.valid() && humanCreateReturn.returnValue()) {
+                                        UserIntroduction.createIntroData(new HumanId(myemail.getObj()));
+
+                                        final String activationURL = new Parameter("http://www.ilikeplaces.com/" + "activate")
+                                                .append(ServletLogin.Username, myemail.getObj(), true)
+                                                .append(ServletLogin.Password,
+                                                        DB.getHumanCRUDHumanLocal(true).doDirtyRHumansAuthentication(new HumanId(myemail.getObj()))
+                                                                .returnValue()
+                                                                .getHumanAuthenticationHash())
+                                                .get();
 
 
-    public static enum TeachTribeIds {
-       teach_tribe_widget
+                                        String htmlBody = Bate.getHTMLStringForOfflineFriendInvite("I Like Places", myemail.getObj());
+
+                                        htmlBody = htmlBody.replace(URL, ElementComposer.generateSimpleLinkTo(activationURL));
+                                        htmlBody = htmlBody.replace(PASSWORD_DETAILS, "Your temporary password is " + randomPassword);
+                                        htmlBody = htmlBody.replace(PASSWORD_ADVICE, "Make sure you change it.");
+
+                                        SendMail.getSendMailLocal().sendAsHTMLAsynchronously(
+                                                myemail.getObj(),
+                                                "I Like Places prides you with an Exclusive Invite!",
+                                                htmlBody);
+
+                                        $$displayNone($$(TeachTribeIds.teach_tribe_signup_section));
+
+                                        TeachTribe.this.notifyUser("Great! Check your email now!");
+
+                                        $$sendJSStmt(JSCodeToSend.redirectPageWithURL(Controller.Page.Activate.getURL()));
+                                    } else {
+                                        TeachTribe.this.notifyUser("Email INVALID!");
+                                    }
+                                } catch (DBDishonourCheckedException e) {
+                                    TeachTribe.this.notifyUser("Email was taken meanwhile!:(");
+                                }
+                            } else {
+                                TeachTribe.this.notifyUser("This email is TAKEN!:(");
+                            }
+                        } else {
+                            TeachTribe.this.notifyUser("Email seems to be wrong :-(");
+                        }
+                    }
+                }
+        );
     }
 }
--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/teach/TeachTribeCriteria.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/teach/TeachTribeCriteria.java
@@ -1,5 +1,8 @@
 package ai.ilikeplaces.logic.Listeners.widgets.teach;
 
+import ai.ilikeplaces.logic.validators.unit.Email;
+import ai.ilikeplaces.logic.validators.unit.HumanId;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Ravindranath Akila
@@ -7,4 +10,67 @@ package ai.ilikeplaces.logic.Listeners.widgets.teach;
  * Time: 8:08 AM
  */
 public class TeachTribeCriteria {
+// ------------------------------ FIELDS ------------------------------
+
+
+// ------------------------------ FIELDS (NON-STATIC)--------------------
+
+
+    private HumanId humanId;
+
+    private SignupCriteria signupCriteria;
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    public TeachTribeCriteria() {
+    }
+
+    public TeachTribeCriteria(final HumanId humanId) {
+
+        this.humanId = humanId;
+    }
+
+    // ------------------------ ACCESSORS / MUTATORS ------------------------
+    public HumanId getHumanId() {
+
+        return humanId;
+    }
+
+    public SignupCriteria getSignupCriteria() {
+
+        return signupCriteria == null ? (signupCriteria = new SignupCriteria()) : signupCriteria;
+    }
+
+
+    // -------------------------- OTHER METHODS --------------------------
+    public TeachTribeCriteria setHumanId(final HumanId humanId) {
+
+        this.humanId = humanId;
+        return this;
+    }
+
+// -------------------------- INNER CLASSES --------------------------
+
+    public class SignupCriteria {
+// ------------------------------ FIELDS ------------------------------
+
+
+// ------------------------------ FIELDS (NON-STATIC)--------------------
+
+
+        private Email email;
+
+        // ------------------------ ACCESSORS / MUTATORS ------------------------
+        public Email getEmail() {
+
+            return email == null ? (email = new Email()) : email;
+        }
+
+        // -------------------------- OTHER METHODS --------------------------
+        public SignupCriteria setEmail(final Email email) {
+
+            this.email = email;
+            return this;
+        }
+    }
 }
