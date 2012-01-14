826ff06f35270a5c0fa70bebe08322e19ad9eb23
vergy <vergy>
2012-01-14 05:33:42 +0000

    [feature #23443083] Down Town Redesigning - Implementation - 1 Autoplay

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/autoplay/AutoplayControlsCriteria.java
@@ -0,0 +1,10 @@
+package ai.ilikeplaces.logic.Listeners.widgets.autoplay;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: 1/13/12
+ * Time: 7:17 PM
+ */
+public class AutoplayControlsCriteria {
+}
