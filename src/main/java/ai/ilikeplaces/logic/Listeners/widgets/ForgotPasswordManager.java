0e720b8ee20f94cbfbe0612a452caf8d24e9a41a
vergy <vergy>
2012-01-28 14:40:30 +0000

    [feature #23210941] Down Town Redesigning - Implementation - 1 Sign In On Widget SIGN IN AND FORGOT PASSWORD TO USE JUST ONE PASSWORD FIELD AND NO CONFIRMATION. FILTER HEADER BROUGHT DOWN TO 2 DAYS.

--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/ForgotPasswordManager.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/ForgotPasswordManager.java
@@ -4,18 +4,15 @@ import ai.ilikeplaces.doc.License;
 import ai.ilikeplaces.doc.NOTE;
 import ai.ilikeplaces.doc.OK;
 import ai.ilikeplaces.entities.HumansAuthentication;
-import ai.ilikeplaces.exception.DBDishonourCheckedException;
 import ai.ilikeplaces.logic.crud.DB;
 import ai.ilikeplaces.logic.mail.SendMail;
 import ai.ilikeplaces.logic.validators.unit.Email;
 import ai.ilikeplaces.logic.validators.unit.ForgotPasswordCode;
 import ai.ilikeplaces.logic.validators.unit.HumanId;
 import ai.ilikeplaces.logic.validators.unit.Password;
-import ai.ilikeplaces.servlets.Controller;
 import ai.ilikeplaces.servlets.Controller.Page;
 import ai.ilikeplaces.servlets.ServletLogin;
 import ai.ilikeplaces.util.*;
-import org.itsnat.core.ItsNatDocument;
 import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.event.NodePropertyTransport;
 import org.itsnat.core.html.ItsNatHTMLDocument;
@@ -27,8 +24,6 @@ import org.w3c.dom.html.HTMLDocument;
 
 import javax.servlet.http.HttpSession;
 import java.util.UUID;
-
-import static ai.ilikeplaces.servlets.Controller.Page.*;
 
 /**
  * @author Ravindranath Akila
@@ -47,12 +42,22 @@ abstract public class ForgotPasswordManager extends AbstractWidgetListener {
     Obj<Boolean> emailset;
     Obj<Boolean> codeCorrect;
     Obj<Boolean> newSet;
-    Obj<Boolean> newConfirmSet;
 
     Password newPass;
-    Password newConfirmPass;
 
     HttpSession httpSession;
+
+    public static enum ForgotPasswordManagerIds implements WidgetIds {
+        ProfileForgotPasswordWidget,
+        ProfileForgotPasswordNotice,
+        ProfileForgotPasswordEmailAddress,
+        ProfileForgotPasswordCodeMail,
+        ProfileForgotPasswordEmailedCode,
+        ProfileForgotPasswordNew,
+        ProfileForgotPasswordSave,
+        ProfileForgotPasswordEmailAddressSection,
+        ProfileForgotPasswordEmailRecoverySection,
+    }
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
@@ -77,20 +82,18 @@ abstract public class ForgotPasswordManager extends AbstractWidgetListener {
         this.emailset = new Obj<Boolean>(false);
         this.codeCorrect = new Obj<Boolean>(false);
         this.newSet = new Obj<Boolean>(false);
-        this.newConfirmSet = new Obj<Boolean>(false);
 
         this.newPass = new Password(null);
-        this.newConfirmPass = new Password(null);
 
 
         this.httpSession = (HttpSession) initArgs[0];
 
-        displayBlock($$(ProfileForgotPasswordWidget));
+        displayBlock($$(ForgotPasswordManagerIds.ProfileForgotPasswordWidget));
     }
 
     @Override
     protected void registerEventListeners(final ItsNatHTMLDocument itsNatHTMLDocument__, final HTMLDocument hTMLDocument__) {
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordEmailAddress), EventType.BLUR.toString(), new EventListener() {
+        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailAddress), EventType.BLUR.toString(), new EventListener() {
             private Email myemail = email;
             private Obj<Boolean> myemailset = emailset;
 
@@ -100,14 +103,14 @@ abstract public class ForgotPasswordManager extends AbstractWidgetListener {
                 if (tempemail.validate() == 0) {
                     myemail.setObj(tempemail.getObj());
                     myemailset.setObj(true);
-                    $$(ProfileForgotPasswordNotice).setTextContent("Okay that appears to be a proper email address.");
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Okay that appears to be a proper email address.");
                 } else {
-                    $$(ProfileForgotPasswordNotice).setTextContent("Duh! enter a proper email address!");
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Duh! enter a proper email address!");
                 }
             }
         }, false, new NodePropertyTransport(MarkupTag.TEXTAREA.value()));
 
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordCodeMail), EventType.CLICK.toString(), new EventListener() {
+        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ForgotPasswordManagerIds.ProfileForgotPasswordCodeMail), EventType.CLICK.toString(), new EventListener() {
             private Email myemail = email;
             private Obj<Boolean> myemailset = emailset;
             private HumanId myhumanId = humanId;
@@ -126,30 +129,30 @@ abstract public class ForgotPasswordManager extends AbstractWidgetListener {
                                         code.getObj());
                                 if (mailsent.returnStatus() == 0 && mailsent.returnValue()) {
                                     myhumanId.setObj(myemail.getObj());
-                                    $$(ProfileForgotPasswordNotice).setTextContent("We've emailed you a code. Paste it here. Don't leave this page.");
-                                    $$displayNone($$(Controller.Page.ProfileForgotPasswordEmailAddressSection));
-                                    $$displayBlock($$(Page.ProfileForgotPasswordEmailRecoverySection));
+                                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("We've emailed you a code. Paste it here. Don't leave this page.");
+                                    $$displayNone($$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailAddressSection));
+                                    $$displayBlock($$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailRecoverySection));
                                     remove(evt_.getCurrentTarget(), EventType.CLICK, this);
                                 } else {
-                                    $$(ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
+                                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
                                 }
                             } else {
-                                $$(ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
+                                $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
                             }
                         } else {
-                            $$(ProfileForgotPasswordNotice).setTextContent("Oh! " + myemail.getObj() + " hasn't signed up. Check for spellings once again please.");
+                            $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Oh! " + myemail.getObj() + " hasn't signed up. Check for spellings once again please.");
                         }
                     } else {
-                        $$(ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent(Return.YIKES_SOMETHING_WENT_WRONG);
                     }
                 } else {
-                    $$(ProfileForgotPasswordNotice).setTextContent("Enter a proper email address to send the code to!");
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Enter a proper email address to send the code to!");
                 }
             }
         }, false);
 
 
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordEmailedCode), EventType.BLUR.toString(), new EventListener() {
+        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailedCode), EventType.BLUR.toString(), new EventListener() {
             private Email myemail = email;
             private ForgotPasswordCode mycode = code;
             private Obj<Boolean> mycodeCorrect = codeCorrect;
@@ -160,123 +163,81 @@ abstract public class ForgotPasswordManager extends AbstractWidgetListener {
                 if (usersValue.validate() == 0) {
                     if (usersValue.getObj().equals(mycode.getObj()) || usersValue.getObj().trim().equals(mycode.getObj())) {
                         mycodeCorrect.setObj(true);
-                        $$(ProfileForgotPasswordNotice).setTextContent("Good, that is the code we emailed you. Now select a new password.");
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Good, that is the code we emailed you. Now select a new password.");
                     } else {
                         mycodeCorrect.setObj(false);//defensive as this value is set above
-                        $$(ProfileForgotPasswordNotice).setTextContent("Oh! That isn't the code we emailed you. Try again");
-                        $$displayBlock($$(Controller.Page.ProfileForgotPasswordEmailAddressSection));
-                        $$displayNone($$(Page.ProfileForgotPasswordEmailRecoverySection));
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Oh! That isn't the code we emailed you. Try again");
+                        //$$displayBlock($$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailAddressSection));
+                        //$$displayNone($$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailRecoverySection));
                     }
                 } else {
-                    $$(ProfileForgotPasswordNotice).setTextContent(usersValue.getViolationAsString());
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent(usersValue.getViolationAsString());
                 }
             }
         }, false, new NodePropertyTransport(MarkupTag.TEXTAREA.value()));
 
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordNew), EventType.BLUR.toString(), new EventListener() {
+        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ForgotPasswordManagerIds.ProfileForgotPasswordNew), EventType.BLUR.toString(), new EventListener() {
             private HumanId myhumanId = humanId;
             private Obj<Boolean> mycodeCorrect = codeCorrect;
             private Password mynewPass = newPass;
 
             private Obj<Boolean> mynewSet = newSet;
-            private Obj<Boolean> mynewConfirmSet = newConfirmSet;
-
             @Override
             public void handleEvent(final Event evt_) {
                 final Password profilePasswordNew = new Password($$(evt_).getAttribute(MarkupTag.TEXTAREA.value()));
                 if (profilePasswordNew.validate() != 0) {
                     mynewSet.setObj(false);
-                    $$(ProfileForgotPasswordNotice).setTextContent(profilePasswordNew.getViolationAsString());
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent(profilePasswordNew.getViolationAsString());
                 } else {
                     mynewSet.setObj(true);
                     if (mycodeCorrect.getObj()) {
                         mynewPass.setObj(profilePasswordNew.getObj());
-                        $$(ProfileForgotPasswordNotice).setTextContent("New Password Valid!");
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("New Password Valid!");
                     } else {
-                        $$(ProfileForgotPasswordNotice).setTextContent("Sorry! You have to provide the code first.");
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Sorry! You have to provide the code first.");
                     }
                 }
             }
         }, false, new NodePropertyTransport(MarkupTag.TEXTAREA.value()));
 
 
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordNewConfirm), EventType.BLUR.toString(), new EventListener() {
+
+        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ForgotPasswordManagerIds.ProfileForgotPasswordSave), EventType.CLICK.toString(), new EventListener() {
             private HumanId myhumanId = humanId;
             private Obj<Boolean> mycodeCorrect = codeCorrect;
             private Obj<Boolean> mynewSet = newSet;
-            private Obj<Boolean> mynewConfirmSet = newConfirmSet;
 
             private Password mynewPass = newPass;
-            private Password mynewConfirmPass = newConfirmPass;
-
-            @Override
-            public void handleEvent(final Event evt_) {
-                final Password profilePasswordNewConfirm = new Password($$(evt_).getAttribute(MarkupTag.TEXTAREA.value()));
-                if (profilePasswordNewConfirm.validate() != 0) {
-                    mynewConfirmSet.setObj(false);
-                    $$(ProfileForgotPasswordNotice).setTextContent(profilePasswordNewConfirm.getViolationAsString());
-                } else {
-                    mynewConfirmSet.setObj(true);
-                    if (mycodeCorrect.getObj()) {
-                        if (mynewSet.getObj() && mynewPass.getObj().equals(profilePasswordNewConfirm.getObj())) {
-                            mynewConfirmPass.setObj(profilePasswordNewConfirm.getObj());
-                            $$(ProfileForgotPasswordNotice).setTextContent("Confirmation Correct!");
-                        } else {
-                            mynewConfirmSet.setObj(false);
-                            $$(ProfileForgotPasswordNotice).setTextContent("Sorry! Confirmation Wrong.");
-                        }
-                    } else {
-                        $$(ProfileForgotPasswordNotice).setTextContent("Sorry! You have to provide the code first.");
-                    }
-                }
-            }
-        }, false, new NodePropertyTransport(MarkupTag.TEXTAREA.value()));
-
-
-        itsNatHTMLDocument__.addEventListener((EventTarget) $$(ProfileForgotPasswordSave), EventType.CLICK.toString(), new EventListener() {
-            private HumanId myhumanId = humanId;
-            private Obj<Boolean> mycodeCorrect = codeCorrect;
-            private Obj<Boolean> mynewSet = newSet;
-            private Obj<Boolean> mynewConfirmSet = newConfirmSet;
-
-            private Password mynewPass = newPass;
-            private Password mynewConfirmPass = newConfirmPass;
 
             private HttpSession myhttpSession = httpSession;
 
             @Override
             public void handleEvent(final Event evt_) {
                 if (!codeCorrect.getObj()) {
-                    $$(ProfileForgotPasswordNotice).setTextContent("Sorry! You have to enter the code.");
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Sorry! You have to enter the code.");
                 } else if (!mynewSet.getObj()) {
-                    $$(ProfileForgotPasswordNotice).setTextContent("Sorry! You have to re-enter new password.");
-                } else if (!mynewConfirmSet.getObj()) {
-                    $$(ProfileForgotPasswordNotice).setTextContent("Sorry! You have to re-enter the confirmation.");
+                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Sorry! You have to enter new password.");
                 } else {
-                    if (!mynewPass.getObj().equals(mynewConfirmPass.getObj())) {
-                        $$(ProfileForgotPasswordNotice).setTextContent("Sorry! Your new password and its' confirmation do not match.");
-                    } else {
-                        final Return<Boolean> r = ServletLogin.changePassword(myhumanId, mynewPass);
-                        if (r.returnStatus() == 0) {
-                            if (r.returnValue()) {
-                                $$(ProfileForgotPasswordNotice).setTextContent("Password Updated Successfully! Please login.");
-                                $$displayNone($$(Page.ProfileForgotPasswordEmailRecoverySection));
+                    final Return<Boolean> r = ServletLogin.changePassword(myhumanId, mynewPass);
+                    if (r.returnStatus() == 0) {
+                        if (r.returnValue()) {
+                            $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Password Updated Successfully! Please login.");
+                            $$displayNone($$(ForgotPasswordManagerIds.ProfileForgotPasswordEmailRecoverySection));
 
-                                AttemptToUnlockAccountForPeopleWhoFailedToDoSoWithEmail:
-                                {
-                                    final Return<Boolean> rAct = DB.getHumanCRUDHumanLocal(true).doUActivateHuman(new HumanId(myhumanId.getHumanId()).getSelfAsValid());
-                                    if (rAct.returnStatus() == 0 && rAct.returnValue()) {
-                                        //Please DO NOT CHANGE THIS unless you know what you are doing!
-                                    } else {
-                                        $$(ProfileForgotPasswordNotice).setTextContent("Password Updated Successfully But Something Went Wrong!");
-                                    }
+                            AttemptToUnlockAccountForPeopleWhoFailedToDoSoWithEmail:
+                            {
+                                final Return<Boolean> rAct = DB.getHumanCRUDHumanLocal(true).doUActivateHuman(new HumanId(myhumanId.getHumanId()).getSelfAsValid());
+                                if (rAct.returnStatus() == 0 && rAct.returnValue()) {
+                                    //Please DO NOT CHANGE THIS unless you know what you are doing!
+                                } else {
+                                    $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Password Updated Successfully But Something Went Wrong!");
                                 }
-                            } else {
-                                $$(ProfileForgotPasswordNotice).setTextContent("Sorry! something went wrong.");
                             }
                         } else {
-                            $$(ProfileForgotPasswordNotice).setTextContent("Sorry! something went wrong.");
+                            $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Sorry! something went wrong.");
                         }
+                    } else {
+                        $$(ForgotPasswordManagerIds.ProfileForgotPasswordNotice).setTextContent("Sorry! something went wrong.");
                     }
                 }
             }
