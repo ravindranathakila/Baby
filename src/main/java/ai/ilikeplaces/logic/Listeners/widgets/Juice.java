4f9da065e46f652d682334140f59f61607a2bf6c
Ravindranath Akila <ravindranathakila@gmail.com>
2012-02-28 20:51:23 +0530

    [feature #25111311] Sign up is generally ok now except that we need to show validation messages properly(needs a bit more testing there).

--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/Juice.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/Juice.java
@@ -3,12 +3,20 @@ package ai.ilikeplaces.logic.Listeners.widgets;
 import ai.ilikeplaces.doc.DOCUMENTATION;
 import ai.ilikeplaces.doc.NOTE;
 import ai.ilikeplaces.doc.SEE;
+import ai.ilikeplaces.logic.contactimports.ImportedContact;
+import ai.ilikeplaces.logic.crud.DB;
+import ai.ilikeplaces.logic.validators.unit.Email;
+import ai.ilikeplaces.logic.validators.unit.HumanId;
+import ai.ilikeplaces.rbs.RBGet;
 import ai.ilikeplaces.servlets.Controller;
-import ai.ilikeplaces.util.AbstractWidgetListener;
+import ai.ilikeplaces.util.*;
 import org.itsnat.core.ItsNatServletRequest;
 import org.itsnat.core.html.ItsNatHTMLDocument;
 import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
 import org.w3c.dom.html.HTMLDocument;
+
+import java.text.MessageFormat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,10 +33,18 @@ import org.w3c.dom.html.HTMLDocument;
                 @NOTE("This also gives us Mixed Fruit Juice rather than a fine blend of one:-/."),
 
                 @NOTE("Excitement: http://www.psychologytoday.com/blog/learning-play/200912/the-nature-excitement"),
-                @NOTE("Excitement (more depth): http://worldcupcollege.com/2010/06/the-psychology-of-excitement/"),
+                @NOTE("Excitement (more depth): http://worldcupcollege.com/2010/06/the-psychology-of-excitement/")
         }
 )
 public class Juice extends AbstractWidgetListener<JuiceCriteria> {
+
+    private static final String ENTER_A_VALID_EMAIL = "enter.a.valid.email";
+    private static final String INVITED_ADDED_0 = "invited.added.0";
+    private static final String COULD_NOT_INVITE_AND_ADD_TRY_AGAIN = "could.not.invite.and.add.try.again";
+    private static final String MESSAGE_ADDING_SELF_AS_FRIEND = "message.adding.self.as.friend";
+    private static final String IS_ALREADY_YOUR_FRIEND = "0.is.already.your.friend";
+    private static final String READ_MORE = "read.more";
+
     public static enum JuiceIds implements WidgetIds {
         Juice_Content,
 
@@ -40,6 +56,11 @@ public class Juice extends AbstractWidgetListener<JuiceCriteria> {
         Juice_Moment_Content,
         Juice_Snap,
         Juice_Snap_Content,
+
+        Juice_Email,
+        Juice_Signup_Click,
+        Juice_Noti,
+
     }
 
 
@@ -49,6 +70,7 @@ public class Juice extends AbstractWidgetListener<JuiceCriteria> {
      */
     public Juice(final ItsNatServletRequest request__, final JuiceCriteria juiceCriteria, final Element appendToElement__) {
         super(request__, Controller.Page.Juice, juiceCriteria, appendToElement__);
+        super.registerUserNotifier(JuiceIds.Juice_Noti);
     }
 
     /**
@@ -61,6 +83,45 @@ public class Juice extends AbstractWidgetListener<JuiceCriteria> {
      */
     @Override
     protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        super.registerForInputText(JuiceIds.Juice_Email, new AIEventListener<JuiceCriteria>(criteria) {
+            /**
+             * Override this method and avoid {@link #handleEvent(org.w3c.dom.events.Event)} to make debug logging transparent
+             *
+             * @param evt fired from client
+             */
+            @Override
+            protected void onFire(Event evt) {
+                Email email__ = new Email(getTargetInputText(evt));
+                if (email__.valid()) {
+                    criteria.getInviteData().setEmail(email__);
+                } else {
+                    notifyUser("Sorry, but we think this email is invalid :-(");
+                }
+            }
+        });
+
+        super.registerForClick(JuiceIds.Juice_Signup_Click, new AIEventListener<JuiceCriteria>(criteria) {
+
+            /**
+             * Override this method and avoid {@link #handleEvent(org.w3c.dom.events.Event)} to make debug logging transparent
+             *
+             * @param evt fired from client
+             */
+            @Override
+            protected void onFire(Event evt) {
+                final Email email = criteria.getInviteData().getEmail();
+
+                if (email.valid()) {
+                    if (!DB.getHumanCRUDHumanLocal(false).doDirtyCheckHuman(email.getObj()).returnValue()) {
+                        final Return<Boolean> returnVal = ai.ilikeplaces.logic.Listeners.widgets.Bate.sendInviteToOfflineInvite(
+                                "I Like Places",
+                                new ImportedContact().setEmail(email.getObj()).setFullName(""));
+                        if (returnVal.valid() || !returnVal.returnValue()) {
+                            notifyUser("Sorry, but we think this email is invalid :-(");
+                        }
+                    }
+                }
+            }
+        });
     }
 }
--- src/main/java/ai/ilikeplaces/logic/Listeners/widgets/JuiceCriteria.java
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/JuiceCriteria.java
@@ -1,5 +1,7 @@
 package ai.ilikeplaces.logic.Listeners.widgets;
 
+import ai.ilikeplaces.logic.validators.unit.Email;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Ravindranath Akila
@@ -7,4 +9,30 @@ package ai.ilikeplaces.logic.Listeners.widgets;
  * Time: 6:48 PM
  */
 public class JuiceCriteria {
+
+    private InviteData inviteData;
+
+
+    public InviteData getInviteData() {
+        return inviteData == null ? (inviteData = new InviteData()) : inviteData;
+    }
+
+    // -------------------------- INNER CLASSES --------------------------
+
+    public class InviteData {
+        // ------------------------------ FIELDS ------------------------------
+
+        private Email email;
+
+        // ------------------------ ACCESSORS / MUTATORS ------------------------
+
+        public Email getEmail() {
+            return email != null ? email : (email = new Email());
+        }
+
+        public InviteData setEmail(final Email email) {
+            this.email = email;
+            return this;
+        }
+    }
 }
