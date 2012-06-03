e5f8431faafda0a606fa3f8bb7b128555d5c64f9
Ravindranath Akila <ravindranathakila@gmail.com>
2012-06-03 12:23:46 +0530

    304913-30602019 [feature] Convert Location Pages To Schema.Org Format - created widgets. now we have to integrate them.

---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/schema/thing/PersonCriteria.java
@@ -0,0 +1,10 @@
+package ai.ilikeplaces.logic.Listeners.widgets.schema.thing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravindranathakila
+ * Date: 6/3/12
+ * Time: 12:06 PM
+ */
+public class PersonCriteria {
+}
---
+++ src/main/java/ai/ilikeplaces/logic/Listeners/widgets/schema/thing/Place.java
@@ -0,0 +1,44 @@
+package ai.ilikeplaces.logic.Listeners.widgets.schema.thing;
+
+import ai.ilikeplaces.servlets.Controller;
+import ai.ilikeplaces.util.AbstractWidgetListener;
+import org.itsnat.core.ItsNatServletRequest;
+import org.itsnat.core.html.ItsNatHTMLDocument;
+import org.w3c.dom.Element;
+import org.w3c.dom.html.HTMLDocument;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravindranathakila
+ * Date: 6/3/12
+ * Time: 12:05 PM
+ */
+public class Place extends AbstractWidgetListener<PlaceCriteria> {
+
+    public static enum PlaceIds implements WidgetIds{
+
+    }
+
+    /**
+     * @param request__
+     * @param page__
+     * @param t
+     * @param appendToElement__
+     */
+    public Place(final ItsNatServletRequest request__, final Controller.Page page__, final PlaceCriteria placeCriteria, final Element appendToElement__) {
+        super(request__, page__, placeCriteria, appendToElement__);
+    }
+
+    /**
+     * Use ItsNatHTMLDocument variable stored in the AbstractListener class
+     * Do not call this method anywhere, just implement it, as it will be
+     * automatically called by the constructor
+     *
+     * @param itsNatHTMLDocument_
+     * @param hTMLDocument_
+     */
+    @Override
+    protected void registerEventListeners(ItsNatHTMLDocument itsNatHTMLDocument_, HTMLDocument hTMLDocument_) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
