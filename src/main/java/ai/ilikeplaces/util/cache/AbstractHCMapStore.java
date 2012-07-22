0f5708984e8eef49e42f594e2b1b227313b5a8f3
Ravindranath Akila <ravindranathakila@gmail.com>
2012-07-22 15:41:03 +0530

    304913-32305273 HBase and Hazelcast integration to support large data sets and distributed caching. Generic MapStore implementation. Entities get persisted twice, need to fix this.

---
+++ src/main/java/ai/ilikeplaces/util/cache/AbstractHCMapStore.java
@@ -0,0 +1,134 @@
+package ai.ilikeplaces.util.cache;
+
+import com.hazelcast.core.MapStore;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravindranathakila
+ * Date: 7/22/12
+ * Time: 2:53 PM
+ */
+public abstract class AbstractHCMapStore<T> implements MapStore<Object, Object> {
+
+    public AbstractHCMapStore() {
+        System.out.println("Starting Hazelcast MapStore implementation:" + this.getClass().getSimpleName());
+    }
+
+    @Override
+    public void store(Object key, Object value) {
+        try {
+            System.out.println("Attempting to store data.");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
+            System.out.println("Obtained EntityManagerFactory.");
+            EntityManager em = emf.createEntityManager();
+            System.out.println("Obtained EntityManager.");
+            em.getTransaction().begin();
+            System.out.println("Began Transaction.");
+            em.merge(value);
+            System.out.println("Persisted data.");
+            em.getTransaction().commit();
+            System.out.println("Committed Transaction");
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+    @Override
+    public void storeAll(Map<Object, Object> keyValueMap) {
+        try {
+            for (final Object key : keyValueMap.keySet()) {
+                store(key, keyValueMap.get(key));
+            }
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+    public void delete(Object key, Class type) {
+        try {
+            System.out.println("Attempting to store data.");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
+            System.out.println("Obtained EntityManagerFactory.");
+            EntityManager em = emf.createEntityManager();
+            System.out.println("Obtained EntityManager.");
+            em.getTransaction().begin();
+            System.out.println("Began Transaction.");
+
+
+            System.out.println("key.getClass()");
+            System.out.println(type);
+
+            em.remove(em.find(type, key));
+
+
+            System.out.println("Removed data.");
+            em.getTransaction().commit();
+            System.out.println("Committed Transaction.");
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+    public void deleteAll(Collection<Object> keys) {
+        try {
+            for (final Object string : keys) {
+                delete(string);
+            }
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+        }
+
+    }
+
+    public Object load(Object key, Class type) {
+        try {
+            System.out.println("Attempting to store data.");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
+            System.out.println("Obtained EntityManagerFactory.");
+            EntityManager em = emf.createEntityManager();
+            System.out.println("Obtained EntityManager.");
+            em.getTransaction().begin();
+            System.out.println("Began Transaction.");
+
+            System.out.println("key.getClass()");
+            System.out.println(key.getClass());
+
+            final Object returnVal = em.find(type, key);
+
+            System.out.println("Loaded data.");
+            em.getTransaction().commit();
+            System.out.println("Committed Transaction.");
+            return returnVal;
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+            throw new RuntimeException(t);
+        }
+    }
+
+    @Override
+    public Map<Object, Object> loadAll(Collection<Object> keys) {
+        try {
+            final Map<Object, Object> returnVal = new HashMap<Object, Object>(keys.size());
+            for (final Object string : keys) {
+                returnVal.put(string, load(string));
+            }
+            return returnVal;
+        } catch (final Throwable t) {
+            t.printStackTrace(System.err);
+            throw new RuntimeException(t);
+        }
+    }
+
+    @Override
+    public Set<Object> loadAllKeys() {
+        return null;
+    }
+}
--- src/main/java/ai/ilikeplaces/util/cache/HazelcastHumanMapStore.java
+++ src/main/java/ai/ilikeplaces/util/cache/HazelcastHumanMapStore.java
@@ -21,110 +21,21 @@ import java.util.Set;
  * Date: 7/3/12
  * Time: 10:40 PM
  */
-public class HazelcastHumanMapStore implements MapStore<String, Human> {
+public class HazelcastHumanMapStore extends AbstractHCMapStore {
 
     public HazelcastHumanMapStore() {
-        System.out.println("Starting Hazelcast MapStore implementation:" + this.getClass().getSimpleName());
     }
 
     @Override
-    public void store(String s, Human human) {
-        try {
-            System.out.println("Attempting to store data.");
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
-            System.out.println("Obtained EntityManagerFactory.");
-            EntityManager em = emf.createEntityManager();
-            System.out.println("Obtained EntityManager.");
-            em.getTransaction().begin();
-            System.out.println("Began Transaction.");
-            em.persist(human);
-            System.out.println("Persisted data.");
-            em.getTransaction().commit();
-            System.out.println("Committed Transaction");
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-        }
+    public void delete(Object o) {
+        super.delete(o, Human.class);
     }
+
 
     @Override
-    public void storeAll(Map<String, Human> stringHumanMap) {
-        try {
-            for (final String key : stringHumanMap.keySet()) {
-                store(key, stringHumanMap.get(key));
-            }
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-        }
+    public Object load(Object o) {
+        return super.load(o, Human.class);
     }
 
-    @Override
-    public void delete(String s) {
-        try {
-            System.out.println("Attempting to store data.");
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
-            System.out.println("Obtained EntityManagerFactory.");
-            EntityManager em = emf.createEntityManager();
-            System.out.println("Obtained EntityManager.");
-            em.getTransaction().begin();
-            System.out.println("Began Transaction.");
-            em.remove(em.find(Human.class, s));
-            System.out.println("Removed data.");
-            em.getTransaction().commit();
-            System.out.println("Committed Transaction.");
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-        }
-    }
 
-    @Override
-    public void deleteAll(Collection<String> strings) {
-        try {
-            for (final String string : strings) {
-                delete(string);
-            }
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-        }
-
-    }
-
-    @Override
-    public Human load(String s) {
-        try {
-            System.out.println("Attempting to store data.");
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
-            System.out.println("Obtained EntityManagerFactory.");
-            EntityManager em = emf.createEntityManager();
-            System.out.println("Obtained EntityManager.");
-            em.getTransaction().begin();
-            System.out.println("Began Transaction.");
-            final Human human = em.find(Human.class, s);
-            System.out.println("Loaded data.");
-            em.getTransaction().commit();
-            System.out.println("Committed Transaction.");
-            return human;
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-            throw new RuntimeException(t);
-        }
-    }
-
-    @Override
-    public Map<String, Human> loadAll(Collection<String> strings) {
-        try {
-            final Map<String, Human> returnVal = new HashMap<String, Human>(strings.size());
-            for (final String string : strings) {
-                returnVal.put(string, load(string));
-            }
-            return returnVal;
-        } catch (final Throwable t) {
-            t.printStackTrace(System.err);
-            throw new RuntimeException(t);
-        }
-    }
-
-    @Override
-    public Set<String> loadAllKeys() {
-        return null;
-    }
 }
