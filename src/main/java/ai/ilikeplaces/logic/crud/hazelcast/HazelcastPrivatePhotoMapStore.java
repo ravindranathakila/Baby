cf4cbd7b4b06d8a741a2f200063f62f36f597dbc
Ravindranath Akila <ravindranathakila@gmail.com>
2012-07-24 11:57:28 +0530

    304913-32305273 HBase and Hazelcast integration to support large data sets and distributed caching. MapStore for individual entity. Note that this commit breaks Hazelcast clients since the HazelcastHumanMapStore has been moved to accompany newly created stores. Proper commit coming up. This is just a safety commit.

---
+++ src/main/java/ai/ilikeplaces/logic/crud/hazelcast/HazelcastPrivatePhotoMapStore.java
@@ -0,0 +1,36 @@
+package ai.ilikeplaces.logic.crud.hazelcast;
+
+import ai.ilikeplaces.entities.Human;
+import ai.ilikeplaces.entities.PrivatePhoto;
+import ai.ilikeplaces.util.cache.AbstractHCMapStore;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravindranathakila
+ * Date: 7/3/12
+ * Time: 10:40 PM
+ */
+public class HazelcastPrivatePhotoMapStore extends AbstractHCMapStore {
+
+    private Class<PrivatePhoto> type = PrivatePhoto.class;
+    ;
+
+    @Override
+    public void store(Object o, Object o1) {
+
+        super.store(o, o1, type);
+    }
+
+    @Override
+    public void delete(Object o) {
+        super.delete(o, type);
+    }
+
+
+    @Override
+    public Object load(Object o) {
+        return super.load(o, type);
+    }
+
+
+}
