6575e2fe293d363a6e5f9abde23053e17d11023c
Ravindranath Akila <ravindranathakila@gmail.com>
2012-10-02 19:57:44 +0800

    304913-36510193 OpenEJB transaction manager locator for Datanucleus jta_locator property. This is however, not used. ~How do we integrated event widgets to attend and share events?

---
+++ src/main/java/ai/ilikeplaces/util/OpenEJBJTALocatorForDatanucleus.java
@@ -0,0 +1,27 @@
+package ai.ilikeplaces.util;
+
+import org.apache.openejb.OpenEJB;
+import org.datanucleus.ClassLoaderResolver;
+import org.datanucleus.jta.TransactionManagerLocator;
+
+import javax.transaction.TransactionManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravindranathakila
+ * Date: 9/29/12
+ * Time: 9:27 PM
+ */
+public class OpenEJBJTALocatorForDatanucleus implements TransactionManagerLocator {
+
+    public static final String FILL = "###################################################################";
+    public static final String OPENEJB_TRANSACTION_MANAGER_TAKENUP = "OpenEJBJTALocatorForDatanucleus is being called by Datanucleus to obtain OpenEJB's transaction manager";
+
+    @Override
+    public TransactionManager getTransactionManager(final ClassLoaderResolver classLoaderResolver) {
+        Loggers.DEBUG.debug(FILL);
+        Loggers.DEBUG.debug(OPENEJB_TRANSACTION_MANAGER_TAKENUP);
+        Loggers.DEBUG.debug(FILL);
+        return OpenEJB.getTransactionManager();
+    }
+}
