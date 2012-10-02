f0d1463fdae88562d337af5567b4e5d628ceac41
Ravindranath Akila <ravindranathakila@gmail.com>
2012-10-02 21:09:25 +0800

    304913-36510193 Fixed the bug. Actually the ThreadLocal implementation was wrong!~How do we integrated event widgets to attend and share events?

--- src/main/java/ai/ilikeplaces/util/EntityManagerInjector.java
+++ src/main/java/ai/ilikeplaces/util/EntityManagerInjector.java
@@ -6,6 +6,7 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 /**
@@ -23,51 +24,67 @@ public class EntityManagerInjector {
         private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("adimpression_ilikeplaces_war_1.6-SNAPSHOTPU_DN");
 
         @Override
-        public EntityManager get() {
+        protected EntityManager initialValue() {
             return entityManagerFactory.createEntityManager();
         }
 
-        @Override
-        public void remove() {
-            get().flush();
-            get().close();
+        public EntityManager setEntityManager(final EntityManager entityManager) {
+            return super.get();
+        }
+
+        public EntityManager getEntityManager() {
+            return super.get();
+        }
+
+        public void removeEntityManager() {
+            super.get().flush();
+            super.get().close();
         }
     };
 
     @AroundInvoke
     public Object injectEntityManager(final InvocationContext invocation) throws Exception {
+        boolean joinedTransaction = false;
 
         try {
             final EntityManager entityManager = THREAD_SAFE_ENTITY_MANAGER.get();
 
-
-//            if (entityManager.getTransaction().isActive()) {
-//                entityManager.joinTransaction();
-//            }
-
-            entityManager.getTransaction().begin();
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.joinTransaction();
+                joinedTransaction = true;
+            } else {
+                entityManager.getTransaction().begin();
+            }
 
 
             Loggers.INFO.info("*****************************");
             Loggers.INFO.info("*****************************");
             Loggers.INFO.info("*****************************");
+            Loggers.INFO.info("***********" + invocation.getMethod().getName() + "************");
             Loggers.INFO.info("***********" + Thread.currentThread().getName() + "************");
             Loggers.INFO.info("***********" + Thread.currentThread().getId() + "************");
             Loggers.INFO.info("***********" + entityManager.toString() + "************");
+            Loggers.INFO.info("***********" + "Joined? " + joinedTransaction + "************");
             Loggers.INFO.info("*****************************");
             Loggers.INFO.info("*****************************");
             Loggers.INFO.info("*****************************");
 
-            return invocation.proceed();
+            final Object proceed = invocation.proceed();
+
+            return proceed;
         } catch (final Throwable t) {
             Loggers.ERROR.error("Problem in transaction", t);
             throw new RuntimeException(t);
         } finally {
-            if (THREAD_SAFE_ENTITY_MANAGER.get().getTransaction().isActive()) {
-                THREAD_SAFE_ENTITY_MANAGER.get().getTransaction().commit();
+            final EntityManager entityManager = THREAD_SAFE_ENTITY_MANAGER.get();
+            final EntityTransaction transaction = entityManager.getTransaction();
+            if (transaction.isActive()) {
+                if (!joinedTransaction) {
+                    transaction.commit();
+                    entityManager.close();
+                    THREAD_SAFE_ENTITY_MANAGER.remove();
+                }
             }
-            //THREAD_SAFE_ENTITY_MANAGER.get().close();
-            //THREAD_SAFE_ENTITY_MANAGER.remove();
         }
 
     }
