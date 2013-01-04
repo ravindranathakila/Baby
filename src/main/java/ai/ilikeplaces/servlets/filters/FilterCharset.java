156384df2232bbf74eb404099ac0cdb347ef9d64
Ravindranath Akila <ravindranathakila@gmail.com>
2013-01-04 20:36:37 +0800

    [feature #41584931] Don't recall encouraging myself to use "if" without braces. Somehow it has happened ~Port Hobbits To Old Persistence Strategy and Fork The Original For NoSQL Testing

--- src/main/java/ai/ilikeplaces/servlets/filters/FilterCharset.java
+++ src/main/java/ai/ilikeplaces/servlets/filters/FilterCharset.java
@@ -22,22 +22,25 @@ import java.io.IOException;
 
 @License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
 public class FilterCharset implements Filter {
-    private String encoding;
     private static final String TEXT_HTML_CHARSET_UTF_8 = "text/html; charset=UTF-8";
     private static final String UTF_8 = "UTF-8";
+    private String encoding;
 
     public void init(FilterConfig config) throws ServletException {
         encoding = config.getInitParameter("requestEncoding");
 
-        if (encoding == null) encoding = UTF_8;
+        if (encoding == null) {
+            encoding = UTF_8;
+        }
     }
 
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain next)
             throws IOException, ServletException {
         // Respect the client-specified character encoding
         // (see HTTP specification section 3.4.1)
-        if (null == request.getCharacterEncoding())
+        if (null == request.getCharacterEncoding()) {
             request.setCharacterEncoding(encoding);
+        }
 
 
         /**
